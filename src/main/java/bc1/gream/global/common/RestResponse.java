7c105b894d12ab27426d0ac220e7dce0571a97a6
barami62 <wodbs7758@naver.com>
2024-01-06 23:54:42 +0900

    feat : RestResponse 추가

---
+++ src/main/java/bc1/gream/global/common/RestResponse.java
@@ -0,0 +1,37 @@
+package bc1.gream.global.common;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestResponse<T> implements Serializable {
+
+    private HttpStatus status;
+    private Integer code;
+    private String message;
+    private T data;
+
+    public static <T> RestResponse<T> success(T data) {
+        return RestResponse.<T>builder()
+            .status(ResultCase.SUCCESS.getHttpStatus())
+            .code(ResultCase.SUCCESS.getCode())
+            .message(ResultCase.SUCCESS.getMessage())
+            .data(data)
+            .build();
+    }
+
+    public static <T> RestResponse<T> error(ResultCase ResultCase) {
+        return RestResponse.<T>builder()
+            .status(ResultCase.getHttpStatus())
+            .code(ResultCase.getCode())
+            .message(ResultCase.getMessage())
+            .build();
+    }
+}
