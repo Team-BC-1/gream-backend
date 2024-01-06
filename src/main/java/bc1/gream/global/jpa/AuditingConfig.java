e06e8b21b1373e802343c91f7d90e83a0c03071f
barami62 <wodbs7758@naver.com>
2024-01-06 23:53:59 +0900

    feat : jpa auditing 설정 추가

---
+++ src/main/java/bc1/gream/global/jpa/AuditingConfig.java
@@ -0,0 +1,10 @@
+package bc1.gream.global.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+}
