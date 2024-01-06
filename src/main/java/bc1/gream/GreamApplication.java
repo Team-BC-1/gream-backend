81a73548557b54ba2e24f7adbd8cbdaaec4f63d8
barami62 <wodbs7758@naver.com>
2024-01-06 23:51:38 +0900

    feat : first init

---
+++ src/main/java/bc1/gream/GreamApplication.java
@@ -0,0 +1,12 @@
+package bc1.gream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreamApplication.class, args);
+    }
+}
