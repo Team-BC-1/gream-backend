9264ae145e8f4fd0b30f0a1b3ae718cd30e9a45f
barami62 <wodbs7758@naver.com>
2024-01-07 21:15:50 +0900

    [#3] fix : 필드 에러 응답 DTO 로 응답 수정

--- src/main/java/bc1/gream/global/exception/GlobalExceptionHandler.java
+++ src/main/java/bc1/gream/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package bc1.gream.global.exception;
 
 import bc1.gream.global.common.ErrorResponseDto;
+import bc1.gream.global.common.InvalidInputMapper;
+import bc1.gream.global.common.InvalidInputResponseDto;
 import bc1.gream.global.common.RestResponse;
 import bc1.gream.global.common.ResultCase;
 import java.util.List;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,9 +14,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public RestResponse<List<ObjectError>> handlerValidationException(MethodArgumentNotValidException ex) {
-        BindingResult bindingResult = ex.getBindingResult();
-        return RestResponse.error(ResultCase.INVALID_INPUT, bindingResult.getAllErrors());
+    public RestResponse<List<InvalidInputResponseDto>> handlerValidationException(MethodArgumentNotValidException ex) {
+        List<InvalidInputResponseDto> invalidInputList = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(InvalidInputMapper.INSTANCE::toInvalidInputResponseDto)
+            .toList();
+
+        return RestResponse.error(ResultCase.INVALID_INPUT, invalidInputList);
     }
 
     @ExceptionHandler(GlobalException.class)
