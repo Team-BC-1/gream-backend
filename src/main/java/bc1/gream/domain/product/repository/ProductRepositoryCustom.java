7a5a8ce7917e26a1273133652f3f22cf99121297
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-07 22:49:27 +0900

    fix : Product findAllByPaging 반환 타입 수정

--- src/main/java/bc1/gream/domain/product/repository/ProductRepositoryCustom.java
+++ src/main/java/bc1/gream/domain/product/repository/ProductRepositoryCustom.java
@@ -3,6 +3,7 @@ package bc1.gream.domain.product.repository;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.service.query.unit.ProductCondition;
 import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ProductRepositoryCustom {
@@ -11,5 +12,5 @@ public interface ProductRepositoryCustom {
     List<Product> findAllBy(ProductCondition condition);
 
     // 전체 조회
-    List<Product> findAllByPaging(ProductCondition condition, Pageable pageable);
+    Page<Product> findAllByPaging(ProductCondition condition, Pageable pageable);
 }
--- src/main/java/bc1/gream/domain/product/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/bc1/gream/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -10,7 +10,9 @@ import bc1.gream.global.config.QueryDslConfig;
 import com.querydsl.core.types.OrderSpecifier;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -32,12 +34,12 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
     }
 
     @Override
-    public List<Product> findAllByPaging(ProductCondition condition, Pageable pageable) {
+    public Page<Product> findAllByPaging(ProductCondition condition, Pageable pageable) {
         // Get Order By Columns
         OrderSpecifier[] ordersOf = ProductQueryOrderFactory.getOrdersOf(pageable.getSort());
 
         // Query + Order + Paging
-        qd.query()
+        List<Product> products = qd.query()
             .selectFrom(product)
             .where(
                 ProductQueryConditionFactory.brandEquals(condition.brand()),
@@ -49,6 +51,6 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
             .limit(pageable.getPageSize())
             .fetch();
 
-        return null;
+        return PageableExecutionUtils.getPage(products, pageable, () -> products.size());
     }
 }
