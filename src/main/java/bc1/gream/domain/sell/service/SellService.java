774bad98019c38661296cc27fd6a066c32cdf0cd
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2024-01-08 03:01:21 +0900

    [#18] feat: 판매 입찰 서비스 인터페이스 추가

--- src/main/java/bc1/gream/domain/sell/service/SellService.java
+++ src/main/java/bc1/gream/domain/sell/service/SellService.java
@@ -1,10 +1,14 @@
 package bc1.gream.domain.sell.service;
 
+import bc1.gream.domain.sell.dto.request.SellBidRequestDto;
 import bc1.gream.domain.sell.dto.request.SellNowRequestDto;
+import bc1.gream.domain.sell.dto.response.SellBidResponseDto;
 import bc1.gream.domain.sell.dto.response.SellNowResponseDto;
 import bc1.gream.domain.user.entity.User;
 
 public interface SellService {
 
     SellNowResponseDto sellNowProduct(User user, SellNowRequestDto requestDto, Long productId);
+
+    SellBidResponseDto sellBidProduct(User user, SellBidRequestDto requestDto, Long productId);
 }
