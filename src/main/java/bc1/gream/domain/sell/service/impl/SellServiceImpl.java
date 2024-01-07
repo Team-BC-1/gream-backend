69aa8928b4cb08e37af3c50a4bc75250aea5f992
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2024-01-08 03:01:57 +0900

    [#18] feat: 판매 입찰 서비스 API 구현 추가 및 Dto 이름 변경 반영

--- src/main/java/bc1/gream/domain/sell/service/impl/SellServiceImpl.java
+++ src/main/java/bc1/gream/domain/sell/service/impl/SellServiceImpl.java
@@ -4,8 +4,10 @@ import static java.time.LocalDateTime.now;
 
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.repository.ProductRepository;
-import bc1.gream.domain.sell.dto.request.SellRequestDto;
-import bc1.gream.domain.sell.dto.response.SellResponseDto;
+import bc1.gream.domain.sell.dto.request.SellBidRequestDto;
+import bc1.gream.domain.sell.dto.request.SellNowRequestDto;
+import bc1.gream.domain.sell.dto.response.SellBidResponseDto;
+import bc1.gream.domain.sell.dto.response.SellNowResponseDto;
 import bc1.gream.domain.sell.entity.Sell;
 import bc1.gream.domain.sell.repository.SellRepository;
 import bc1.gream.domain.sell.service.SellService;
@@ -27,18 +29,25 @@ public class SellServiceImpl implements SellService {
 
     @Override
     @Transactional
-    public SellResponseDto sellNowProduct(User user, SellRequestDto requestDto, Long productId) {
+    public SellNowResponseDto sellNowProduct(User user, SellNowRequestDto requestDto, Long productId) {
         Sell sell = nowSellProduct(user, requestDto, productId);
         Sell savedSell = sellRepository.save(sell);
 
         return SellServiceMapper.INSTANCE.toSellNowResponseDto(savedSell);
     }
 
-    private Sell nowSellProduct(User user, SellRequestDto requestDto, Long productId) {
+    @Override
+    public SellBidResponseDto sellBidProduct(User user, SellBidRequestDto requestDto, Long productId) {
+        Sell sell = bidSellProduct(user, requestDto, productId);
+        Sell savedSell = sellRepository.save(sell);
+        return SellServiceMapper.INSTANCE.toSellBidResponseDto(savedSell);
+    }
+
+    private Sell nowSellProduct(User user, SellNowRequestDto requestDto, Long productId) {
 
         Long price = requestDto.price();
         Boolean isClose = true;
-        LocalDateTime deadLineAt = now().plusDays(7);
+        LocalDateTime deadLineAt = now();
         Product product = findProductById(productId);
         Boolean isNow = true;
         String paymentType = requestDto.paymentType();
@@ -54,10 +63,34 @@ public class SellServiceImpl implements SellService {
             .build();
     }
 
+    private Sell bidSellProduct(User user, SellBidRequestDto requestDto, Long productId) {
+
+        Long price = requestDto.price();
+        Boolean isClose = false;
+        LocalDateTime deadLineAt = now().plusDays(7);
+        if (requestDto.period() != null) {
+            deadLineAt = now().plusDays(requestDto.period());
+        }
+        Product product = findProductById(productId);
+        Boolean isNow = false;
+        String paymentType = requestDto.paymentType();
+
+        return Sell.builder()
+            .price(price)
+            .isClose(isClose)
+            .deadlineAt(deadLineAt)
+            .isNow(isNow)
+            .paymentType(paymentType)
+            .product(product)
+            .user(user)
+            .build();
+    }
+
 
     private Product findProductById(Long productId) {
         return productRepository.findById(productId).orElseThrow(
             () -> new GlobalException(ResultCase.PRODUCT_NOT_FOUND)
         );
     }
+
 }
