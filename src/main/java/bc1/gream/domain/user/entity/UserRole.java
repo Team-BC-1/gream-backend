c1936395d415e8c2bae5a1dfaaf390f69c832a5d
barami62 <wodbs7758@naver.com>
2024-01-07 19:52:35 +0900

    [#3] feat : 유저 엔티티 추가

---
+++ src/main/java/bc1/gream/domain/user/entity/UserRole.java
@@ -0,0 +1,14 @@
+package bc1.gream.domain.user.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    ADMIN("ROLE_ADMIN", "관리자"),
+    USER("ROLE_USER", "사용자");
+
+    private final String authority;
+    private final String value;
+}
