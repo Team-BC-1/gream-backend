db8c49290f65a12a5c19f0b3ae2102123d813005
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-07 22:12:29 +0900

    feat : QueryDsl 설정

---
+++ src/main/java/bc1/gream/global/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package bc1.gream.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory query() {
+        return new JPAQueryFactory(em);
+    }
+
+}
