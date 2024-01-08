ec698d9d8eab12e5e5e6040a990bff2dfced28a0
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-08 15:41:54 +0900

    feat : Order Entity 수정

--- src/main/java/bc1/gream/domain/order/entity/Order.java
+++ src/main/java/bc1/gream/domain/order/entity/Order.java
@@ -1,65 +1,53 @@
 package bc1.gream.domain.order.entity;
 
 import bc1.gream.domain.model.BaseEntity;
-import bc1.gream.domain.product.entity.Product;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tb_order")
 @Getter
-@Builder
 @DynamicUpdate
 @DynamicInsert
 public class Order extends BaseEntity {
-
-//    @ManyToOne
-//    private 판매입찰 판매입찰;
-
-//    @ManyToOne
-//    private 구매입찰 구매입찰;
-
-//    @OneToOne
-//    @JoinColumn(name = "coupon_id")
-//    private Coupon coupon;
-
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
+    @Column
     @NotNull
-    private OrderState state;
+    private BigInteger priceToSell;
 
     @Column
     @NotNull
-    private BigInteger buyPrice;
+    private BigInteger finalPrice;
 
-    private LocalDateTime tradeDate;
+    @Column
+    @NotNull
+    private BigInteger salePurchasedPrice;
 
-    private Boolean isInstantTrade;
+    private LocalDateTime orderedAt;
+
+    @Builder
+    private Order(BigInteger priceToSell, BigInteger finalPrice, BigInteger salePurchasedPrice, LocalDateTime orderedAt) {
+        this.priceToSell = priceToSell;
+        this.finalPrice = finalPrice;
+        this.salePurchasedPrice = salePurchasedPrice;
+        this.orderedAt = orderedAt;
+    }
 }
