5359eb4309ac505bceba06cd1b0025e9dad5a7b1
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-09 00:08:24 +0900

    feat : 관심상품 추가 삭제 컨트롤러 구현

--- src/main/java/bc1/gream/domain/product/service/command/ProductCommendService.java
+++ src/main/java/bc1/gream/domain/product/service/command/ProductCommendService.java
@@ -1,5 +1,10 @@
 package bc1.gream.domain.product.service.command;
 
+import bc1.gream.domain.user.entity.User;
+
 public interface ProductCommendService {
-    // 관심상품 등록
+
+    void likeProduct(User user, Long productId);
+
+    void dislikeProduct(User user, Long productId);
 }
