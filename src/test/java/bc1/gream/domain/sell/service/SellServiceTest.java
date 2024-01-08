18ddc7e0399d5dd59261cbbdc7920e9459290ad5
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2024-01-08 22:02:21 +0900

    [#11] test: User를 따로선언하지않고 테스트스텁에 있는 것으로 활용

--- src/test/java/bc1/gream/domain/sell/service/SellServiceTest.java
+++ src/test/java/bc1/gream/domain/sell/service/SellServiceTest.java
@@ -6,17 +6,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import bc1.gream.domain.product.entity.Product;
+import bc1.gream.domain.product.repository.ProductRepository;
 import bc1.gream.domain.sell.dto.request.SellBidRequestDto;
 import bc1.gream.domain.sell.dto.response.SellBidResponseDto;
 import bc1.gream.domain.sell.entity.Sell;
 import bc1.gream.domain.sell.repository.SellRepository;
 import bc1.gream.domain.user.entity.User;
 import bc1.gream.test.SellTest;
+import java.io.IOException;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @ExtendWith(MockitoExtension.class)
 class SellServiceTest implements SellTest {
@@ -24,24 +29,31 @@ class SellServiceTest implements SellTest {
     @Mock
     SellRepository sellRepository;
 
+    @Mock
+    ProductRepository productRepository;
+
     @InjectMocks
     SellService sellService;
 
     @Test
-    void sellBidProductTest() {
+    void sellBidProductTest() throws IOException {
 
         // given
-        Product product = TEST_PRODUCT;
-        User user = TEST_USER;
+        String url = "images/images.png";
+        Resource fileResource = new ClassPathResource(url);
+
+        System.out.println(fileResource.getURL());
+
         SellBidRequestDto requestDto = SellBidRequestDto.builder()
             .price(TEST_SELL_PRICE)
-            .gifticonUrl("C:\\Users\\Hwnag\\Desktop\\image")
+            .gifticonUrl(String.valueOf(fileResource.getURL()))
             .build();
 
+        given(productRepository.findById(1L)).willReturn(Optional.of(TEST_PRODUCT));
         given(sellRepository.save(any(Sell.class))).willReturn(TEST_SELL);
 
         // when
-        SellBidResponseDto responseDto = sellService.sellBidProduct(user, requestDto, product);
+        SellBidResponseDto responseDto = sellService.sellBidProduct(TEST_USER, requestDto, TEST_PRODUCT_ID);
 
         // then
         assertThat(responseDto.price()).isEqualTo(TEST_SELL_PRICE);
