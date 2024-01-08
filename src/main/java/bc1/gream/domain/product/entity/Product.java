b4334db7d7f6c77863efba9ceaf056b04038c346
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2024-01-08 21:59:06 +0900

    [#11] fix: 상품 엔티티에 AccessLevel.PROTECTED 걸고 @Build로 접근하게 설정

--- src/main/java/bc1/gream/domain/product/entity/Product.java
+++ src/main/java/bc1/gream/domain/product/entity/Product.java
@@ -1,17 +1,19 @@
 package bc1.gream.domain.product.entity;
 
 import bc1.gream.domain.model.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tb_product")
 public class Product extends BaseEntity {
@@ -19,4 +21,11 @@ public class Product extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "brand")
+    private String brand;
+
+    @Builder
+    public Product(String brand) {
+        this.brand = brand;
+    }
 }
