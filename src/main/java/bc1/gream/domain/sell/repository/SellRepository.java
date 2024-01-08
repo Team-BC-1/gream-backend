549f58c135bfb68d675b1d663aeed9af8c66686f
hwang <ghkdrbwjd369@naver.com>
2024-01-08 18:30:25 +0900

    [#11] feat: 입찰 판매 기능 레파지토리 추가

---
+++ src/main/java/bc1/gream/domain/sell/repository/SellRepository.java
@@ -0,0 +1,8 @@
+package bc1.gream.domain.sell.repository;
+
+import bc1.gream.domain.sell.entity.Sell;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellRepository extends JpaRepository<Sell, Long> {
+
+}
