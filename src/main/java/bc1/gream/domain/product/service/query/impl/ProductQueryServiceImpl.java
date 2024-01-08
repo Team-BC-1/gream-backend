1d6a329acdf6459f5a9c79f7a35932e87ec438c8
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-09 00:05:35 +0900

    feat : ProductQueryService 상품단건조회

--- src/main/java/bc1/gream/domain/product/service/query/impl/ProductQueryServiceImpl.java
+++ src/main/java/bc1/gream/domain/product/service/query/impl/ProductQueryServiceImpl.java
@@ -4,9 +4,12 @@ import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.repository.ProductRepository;
 import bc1.gream.domain.product.service.query.ProductQueryService;
 import bc1.gream.domain.product.service.query.unit.ProductCondition;
+import bc1.gream.global.common.ResultCase;
+import bc1.gream.global.exception.GlobalException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,22 +19,23 @@ public class ProductQueryServiceImpl implements ProductQueryService {
     private final ProductRepository productRepository;
 
     @Override
-    public List<Product> findAllBy() {
+    public List<Product> findAll() {
         return productRepository.findAll();
     }
 
     @Override
     public List<Product> findAllBy(ProductCondition condition) {
-        return null;
+        return productRepository.findAllBy(condition);
     }
 
     @Override
-    public Page<Product> findAllByPaging(ProductCondition condition) {
-        return null;
+    public Page<Product> findAllByPaging(ProductCondition condition, Pageable pageable) {
+        return productRepository.findAllByPaging(condition, pageable);
     }
 
     @Override
     public Product findBy(Long id) {
-        return null;
+        return productRepository.findById(id)
+            .orElseThrow(() -> new GlobalException(ResultCase.PRODUCT_NOT_FOUND));
     }
 }
