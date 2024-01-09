16f45678680d1b23683382bda74b8e2566b5b432
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-09 14:35:09 +0900

    feat : Product 에 대한 Order 리스트 조회 기능 구현

--- src/main/java/bc1/gream/domain/order/controller/query/OrderQueryController.java
+++ src/main/java/bc1/gream/domain/order/controller/query/OrderQueryController.java
@@ -1,5 +1,10 @@
 package bc1.gream.domain.order.controller.query;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderQueryController {
 
 }
--- src/main/java/bc1/gream/domain/order/service/query/OrderQueryService.java
+++ src/main/java/bc1/gream/domain/order/service/query/OrderQueryService.java
@@ -1,5 +1,10 @@
 package bc1.gream.domain.order.service.query;
 
-public class OrderQueryService {
+import bc1.gream.domain.order.entity.Order;
+import bc1.gream.domain.product.entity.Product;
+import java.util.List;
 
+public interface OrderQueryService {
+
+    List<Order> findAllTradesOf(Product product);
 }
