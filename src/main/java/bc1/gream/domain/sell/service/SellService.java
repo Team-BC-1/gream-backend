ea52740c33b79a3622f57d68f18e8bc699ca6279
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-10 15:58:27 +0900

    feat : 판매입찰가 내역 서비스

--- src/main/java/bc1/gream/domain/sell/service/SellService.java
+++ src/main/java/bc1/gream/domain/sell/service/SellService.java
@@ -1,23 +1,25 @@
 package bc1.gream.domain.sell.service;
 
-import static bc1.gream.global.common.ResultCase.*;
+import static bc1.gream.global.common.ResultCase.PRODUCT_NOT_FOUND;
 
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.repository.ProductRepository;
-import bc1.gream.domain.product.service.ProductService;
 import bc1.gream.domain.sell.dto.request.SellBidRequestDto;
 import bc1.gream.domain.sell.dto.response.SellBidResponseDto;
 import bc1.gream.domain.sell.entity.Sell;
+import bc1.gream.domain.sell.mapper.SellMapper;
 import bc1.gream.domain.sell.repository.SellRepository;
 import bc1.gream.domain.user.entity.User;
-import bc1.gream.global.common.ResultCase;
 import bc1.gream.global.exception.GlobalException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -42,7 +44,7 @@ public class SellService {
 
         Sell savedSell = sellRepository.save(sell);
 
-        return SellServiceMapper.INSTANCE.toSellBidResponseDto(savedSell);
+        return SellMapper.INSTANCE.toSellBidResponseDto(savedSell);
     }
 
     private Integer getPeriod(Integer period) {
@@ -53,5 +55,17 @@ public class SellService {
         return productRepository.findById(productId).orElseThrow(
             () -> new GlobalException(PRODUCT_NOT_FOUND)
         );
+    }
+
+    /**
+     * Product에 대한 판매입찰가 내역 페이징 조회
+     *
+     * @param product  이모티콘 상품
+     * @param pageable 페이징 요청 데이터
+     * @return 판매입찰가 내역 페이징 데이터
+     */
+    @Transactional(readOnly = true)
+    public Page<Sell> findAllSellBidsOf(Product product, Pageable pageable) {
+        return sellRepository.findAllPricesOf(product, pageable);
     }
 }
