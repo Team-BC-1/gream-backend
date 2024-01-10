1fdf494db5600b826aac0efc65b023a8cd699945
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-10 11:05:46 +0900

    [#10] fix : script 수정

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ fi
 
 echo "> $JAR_PATH deploy"
 cd /build/libs
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH --spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
