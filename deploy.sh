777bc9f84ba39eff3e30c2c4159471fcea07c49d
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-10 11:55:10 +0900

    [#10] fix : plain.jar생성을 막기위함 및  스크립트 실행

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,24 @@ REPOSITORY=/home/ubuntu/gream
 cd $REPOSITORY
 
 # shellcheck disable=SC2010
-JAR_NAME=$(ls $REPOSITORY/ | grep 'SNAPSHOT.jar' | tail -n 1)
-echo "$JAR_NAME"
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-echo "$JAR_PATH"
+CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
 
-CURRENT_PID=$(pgrep -f jar)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> Nothing to end."
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
-chmod +x $JAR_PATH
-echo "> $JAR_PATH deploy"
-nohup java -jar $JAR_PATH --spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
