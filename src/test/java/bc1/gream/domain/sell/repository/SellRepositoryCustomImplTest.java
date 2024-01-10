e5282a60e62afceb7b0d9a1a15036dec5538cf0b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-10 16:59:01 +0900

    test : SellRepositoryCustomImpl 판매입찰 페이징 조회 Test

---
+++ src/test/java/bc1/gream/domain/sell/repository/SellRepositoryCustomImplTest.java
@@ -0,0 +1,61 @@
+package bc1.gream.domain.sell.repository;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import bc1.gream.domain.product.repository.ProductRepository;
+import bc1.gream.domain.sell.entity.Sell;
+import bc1.gream.domain.user.repository.UserRepository;
+import bc1.gream.global.config.QueryDslConfig;
+import bc1.gream.global.jpa.AuditingConfig;
+import bc1.gream.test.SellTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import({QueryDslConfig.class, AuditingConfig.class})
+class SellRepositoryCustomImplTest implements SellTest {
+
+    @Autowired
+    private SellRepositoryCustomImpl sellRepositoryCustom;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private SellRepository sellRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(TEST_USER);
+        productRepository.save(TEST_PRODUCT);
+        sellRepository.save(TEST_SELL);
+    }
+
+    @Test
+    @DisplayName("상품에 대한 판매입찰 인스턴스를 조회, 페이징합니다.")
+    public void 상품_판매입찰_조회_페이징() {
+        // GIVEN
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
+
+        // WHEN
+        Page<Sell> allPricesOf = sellRepositoryCustom.findAllPricesOf(TEST_PRODUCT, pageable);
+
+        // THEN
+        boolean hasSellBid = allPricesOf.stream()
+            .anyMatch(buy -> buy.equals(TEST_SELL));
+        assertTrue(hasSellBid);
+    }
+}
