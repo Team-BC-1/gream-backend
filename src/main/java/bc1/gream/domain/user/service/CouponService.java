924c08d4a12dd57954f86e64bd61dc1a80afaac5
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2024-01-10 17:45:34 +0900

    [#39] feat: 쿠폰 상태 변경 서비스 API 추가

---
+++ src/main/java/bc1/gream/domain/user/service/CouponService.java
@@ -0,0 +1,27 @@
+package bc1.gream.domain.user.service;
+
+import static bc1.gream.global.common.ResultCase.COUPON_NOT_FOUND;
+
+import bc1.gream.domain.user.entity.Coupon;
+import bc1.gream.domain.user.entity.CouponStatus;
+import bc1.gream.domain.user.repository.CouponRepository;
+import bc1.gream.global.exception.GlobalException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public void changeCouponStatus(Long couponId, CouponStatus couponStatus) {
+        Coupon coupon = couponRepository.findById(couponId).orElseThrow(
+            () -> new GlobalException(COUPON_NOT_FOUND)
+        );
+
+        coupon.update(couponStatus);
+    }
+}
