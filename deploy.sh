ebddc76a765ad545e95d058cefad25246e9ae14f
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-10 17:18:36 +0900

    [#10] fix : 코드 수정

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,8 @@ REPOSITORY=/home/ubuntu/gream
 # shellcheck disable=SC2164
 cd $REPOSITORY
 
-# shellcheck disable=SC2010
-CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  kill -9 `ps -ef|grep java|awk '{print $2}'`
-  sleep 5
-fi
-
+# shellcheck disable=SC2046
+kill -9 `ps -ef|grep java|awk '{print $2}'`
 
 JAR_NAME=$(ls -tr build/libs/*.jar | tail -n 1)
 
