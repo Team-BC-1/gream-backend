09afff2e7f3beb4ec29e94b3f80d4a7a453af5da
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2024-01-10 16:41:12 +0900

    [#39] feat: 기프티콘 레파지토리 추가

---
+++ src/main/java/bc1/gream/domain/order/repository/GifticonRepository.java
@@ -0,0 +1,8 @@
+package bc1.gream.domain.order.repository;
+
+import bc1.gream.domain.order.entity.Gifticon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GifticonRepository extends JpaRepository<Gifticon, Long> {
+
+}
