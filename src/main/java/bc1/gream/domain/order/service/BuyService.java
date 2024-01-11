1a86324130f252800d03a67678e7348d7ac27f08
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-11 17:26:01 +0900

    [#18] feat: 즉시 구매 기능 서비스 API 추가

--- src/main/java/bc1/gream/domain/order/service/BuyService.java
+++ src/main/java/bc1/gream/domain/order/service/BuyService.java
@@ -1,22 +1,32 @@
 package bc1.gream.domain.order.service;
 
 import static bc1.gream.global.common.ResultCase.BUY_BID_PRODUCT_NOT_FOUND;
+import static bc1.gream.global.common.ResultCase.GIFTICON_NOT_FOUND;
 import static bc1.gream.global.common.ResultCase.NOT_AUTHORIZED;
 import static bc1.gream.global.common.ResultCase.PRODUCT_NOT_FOUND;
+import static bc1.gream.global.common.ResultCase.SELL_BID_PRODUCT_NOT_FOUND;
 
+import bc1.gream.domain.common.facade.FindCouponFacade;
 import bc1.gream.domain.order.dto.request.BuyBidRequestDto;
+import bc1.gream.domain.order.dto.request.BuyNowRequestDto;
 import bc1.gream.domain.order.dto.response.BuyBidResponseDto;
 import bc1.gream.domain.order.dto.response.BuyCancelBidResponseDto;
+import bc1.gream.domain.order.dto.response.BuyNowResponseDto;
 import bc1.gream.domain.order.entity.Buy;
+import bc1.gream.domain.order.entity.Gifticon;
+import bc1.gream.domain.order.entity.Order;
+import bc1.gream.domain.order.entity.Sell;
 import bc1.gream.domain.order.mapper.BuyMapper;
+import bc1.gream.domain.order.mapper.OrderMapper;
 import bc1.gream.domain.order.repository.BuyRepository;
 import bc1.gream.domain.order.repository.GifticonRepository;
 import bc1.gream.domain.order.repository.OrderRepository;
 import bc1.gream.domain.order.repository.SellRepository;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.repository.ProductRepository;
+import bc1.gream.domain.user.entity.Coupon;
+import bc1.gream.domain.user.entity.DiscountType;
 import bc1.gream.domain.user.entity.User;
-import bc1.gream.domain.user.repository.CouponRepository;
 import bc1.gream.global.exception.GlobalException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -32,10 +42,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BuyService {
 
+    private final FindCouponFacade findCouponFacade;
+    
     private final BuyRepository buyRepository;
     private final ProductRepository productRepository;
     private final SellRepository sellRepository;
-    private final CouponRepository couponRepository;
     private final OrderRepository orderRepository;
     private final GifticonRepository gifticonRepository;
 
@@ -64,13 +75,34 @@ public class BuyService {
     public BuyCancelBidResponseDto buyCancelBid(User user, Long buyId) {
         Buy buyBid = findBuyById(buyId);
 
-        if (isNotBuyerLoggedInUser(buyBid, user)) {
+        if (isBuyerLoggedInUser(buyBid, user)) {
             throw new GlobalException(NOT_AUTHORIZED);
         }
 
         buyRepository.delete(buyBid);
 
         return new BuyCancelBidResponseDto();
+    }
+
+    public BuyNowResponseDto buyNowProduct(User user, BuyNowRequestDto requestDto, Long productId) {
+        Long price = requestDto.price();    // finalPrice
+        Sell sell = sellRepository.findByProductIdAndPrice(productId, price).orElseThrow(
+            () -> new GlobalException(SELL_BID_PRODUCT_NOT_FOUND)
+        );
+        Long expectedPrice = calcDiscount(requestDto, user);
+
+        Order order = Order.builder()
+            .product(sell.getProduct())
+            .buyer(user)
+            .seller(sell.getUser())
+            .finalPrice(price)
+            .expectedPrice(expectedPrice)
+            .build();
+
+        Order savedOrder = orderRepository.save(order);
+        orderGifticon(sell.getId(), savedOrder);
+
+        return OrderMapper.INSTANCE.toBuyNowResponseDto(savedOrder);
     }
 
     private Integer getPeriod(Integer period) {
@@ -89,10 +121,30 @@ public class BuyService {
         );
     }
 
-    private boolean isNotBuyerLoggedInUser(Buy buy, User user) {
-        return !buy.getUser().getLoginId().equals(user.getLoginId());
+    private boolean isBuyerLoggedInUser(Buy buy, User user) {
+        return buy.getUser().getLoginId().equals(user.getLoginId());
     }
 
+    private Long calcDiscount(BuyNowRequestDto requestDto, User user) {
+        Coupon coupon = findCouponFacade.findByCouponId(requestDto.couponId(), user);
+        Long expectedPrice = 0L;
+
+        if (coupon.getDiscountType().equals(DiscountType.FIX)) {
+            expectedPrice = requestDto.price() - coupon.getDiscount();
+        } else {
+            expectedPrice = requestDto.price() * (100 - coupon.getDiscount()) / 100;
+        }
+
+        return expectedPrice;
+    }
+
+    private void orderGifticon(Long sellId, Order order) {
+        Gifticon gifticon = gifticonRepository.findBySell_Id(sellId).orElseThrow(
+            () -> new GlobalException(GIFTICON_NOT_FOUND)
+        );
+
+        gifticon.updateOrder(order);
+    }
 
     /**
      * Product에 대한 구매입찰가 내역 페이징 조회
