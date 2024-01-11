f7941e3a7ef2681ab0093ab08b1d3b4becf104c6
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-11 17:13:38 +0900

    [#18] fix: 이전 풀리퀘스트 코멘트 반영

--- src/main/java/bc1/gream/domain/user/entity/Coupon.java
+++ src/main/java/bc1/gream/domain/user/entity/Coupon.java
@@ -3,6 +3,7 @@ package bc1.gream.domain.user.entity;
 import bc1.gream.domain.common.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,13 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_coupon")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Coupon extends BaseEntity {
 
@@ -27,12 +29,14 @@ public class Coupon extends BaseEntity {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Enumerated
     @Column(name = "discount_type", nullable = false)
     private DiscountType discountType;
 
     @Column(nullable = false)
     private Long discount;
 
+    @Enumerated
     @Column
     private CouponStatus status;
 
