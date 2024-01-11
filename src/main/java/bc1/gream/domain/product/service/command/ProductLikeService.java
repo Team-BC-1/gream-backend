80ea9ad0d6cca4b6d0bd63e8cc12ec7833806671
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-11 18:03:55 +0900

    test : ProductLikeService 관심상품 등록 해제 테스트

--- src/main/java/bc1/gream/domain/product/service/command/ProductLikeService.java
+++ src/main/java/bc1/gream/domain/product/service/command/ProductLikeService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProductCommendService {
+public class ProductLikeService {
 
 
     private final ProductRepository productRepository;
