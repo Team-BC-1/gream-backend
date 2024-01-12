9f3ca572e0bd36f641f64962534b60cfd13f3ce4
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-12 20:15:33 +0900

    fix : Gifticon, Order 영속성 전이 Persist로 설정

--- src/test/java/bc1/gream/domain/sell/repository/SellRepositoryCustomImplTest.java
+++ src/test/java/bc1/gream/domain/sell/repository/SellRepositoryCustomImplTest.java
@@ -15,7 +15,6 @@ import bc1.gream.global.config.QueryDslConfig;
 import bc1.gream.global.jpa.AuditingConfig;
 import bc1.gream.test.SellTest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +32,6 @@ import org.springframework.test.context.ActiveProfiles;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Import({QueryDslConfig.class, AuditingConfig.class})
-@Disabled
 class SellRepositoryCustomImplTest implements SellTest {
 
     User user;
