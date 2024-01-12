062a248e50eb404b1fcfa10a4d5bcb0263abd95b
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-12 15:22:45 +0900

    [#59] feat: 즉시 판매 Request / Response 추가

---
+++ src/main/java/bc1/gream/domain/order/dto/request/SellNowRequestDto.java
@@ -0,0 +1,8 @@
+package bc1.gream.domain.order.dto.request;
+
+public record SellNowRequestDto(
+    Long price,
+    String gifticonUrl
+) {
+
+}
---
+++ src/main/java/bc1/gream/domain/order/dto/response/SellNowResponseDto.java
@@ -0,0 +1,8 @@
+package bc1.gream.domain.order.dto.response;
+
+public record SellNowResponseDto(
+    Long orderId,
+    Long price
+) {
+
+}
