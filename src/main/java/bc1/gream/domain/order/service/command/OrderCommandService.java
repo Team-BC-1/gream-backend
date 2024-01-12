572579c11f029b804831827bf7e4803ebcdb04c9
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-12 22:20:01 +0900

    feat : OrderCommandService 구매입찰에 대한 쿠폰적용된 주문 발행 및 저장

--- src/main/java/bc1/gream/domain/order/service/command/OrderCommandService.java
+++ src/main/java/bc1/gream/domain/order/service/command/OrderCommandService.java
@@ -1,5 +1,37 @@
 package bc1.gream.domain.order.service.command;
 
+import bc1.gream.domain.order.entity.Buy;
+import bc1.gream.domain.order.entity.Order;
+import bc1.gream.domain.order.repository.OrderRepository;
+import bc1.gream.domain.order.service.helper.CouponCalculator;
+import bc1.gream.domain.user.entity.Coupon;
+import bc1.gream.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrderCommandService {
 
+    private final OrderRepository orderRepository;
+
+    /**
+     * 구매입찰에 대한 쿠폰적용된 주문 발행 및 저장
+     *
+     * @param buy    구매입찰
+     * @param seller 판매자
+     * @param coupon 쿠폰
+     * @return 새로운 주문
+     */
+    public Order saveOrderOfBuy(Buy buy, User seller, Coupon coupon) {
+        Long finalPrice = CouponCalculator.calculateDiscount(coupon, buy.getPrice());
+        Order order = Order.builder()
+            .product(buy.getProduct())
+            .buyer(buy.getUser())
+            .seller(seller)
+            .finalPrice(CouponCalculator.calculateDiscount(coupon, buy.getPrice()))
+            .expectedPrice(buy.getPrice())
+            .build();
+        return orderRepository.save(order);
+    }
 }
