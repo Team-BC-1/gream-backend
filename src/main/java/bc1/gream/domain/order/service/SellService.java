6d787a2709f7b4d22227ff9cab6d2c2fd1299219
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-12 15:22:25 +0900

    [#59] feat: 즉시 판매 서비스 API 추가 및 판매와 기프티콘 연관관계 변경으로 인한 수정

--- src/main/java/bc1/gream/domain/order/service/SellService.java
+++ src/main/java/bc1/gream/domain/order/service/SellService.java
@@ -1,18 +1,25 @@
 package bc1.gream.domain.order.service;
 
+import static bc1.gream.global.common.ResultCase.BUY_BID_PRODUCT_NOT_FOUND;
 import static bc1.gream.global.common.ResultCase.GIFTICON_NOT_FOUND;
 import static bc1.gream.global.common.ResultCase.NOT_AUTHORIZED;
 import static bc1.gream.global.common.ResultCase.PRODUCT_NOT_FOUND;
 import static bc1.gream.global.common.ResultCase.SELL_BID_PRODUCT_NOT_FOUND;
 
 import bc1.gream.domain.order.dto.request.SellBidRequestDto;
+import bc1.gream.domain.order.dto.request.SellNowRequestDto;
 import bc1.gream.domain.order.dto.response.SellBidResponseDto;
 import bc1.gream.domain.order.dto.response.SellCancelBidResponseDto;
+import bc1.gream.domain.order.dto.response.SellNowResponseDto;
+import bc1.gream.domain.order.entity.Buy;
 import bc1.gream.domain.order.entity.Gifticon;
 import bc1.gream.domain.order.entity.Order;
 import bc1.gream.domain.order.entity.Sell;
+import bc1.gream.domain.order.mapper.OrderMapper;
 import bc1.gream.domain.order.mapper.SellMapper;
+import bc1.gream.domain.order.repository.BuyRepository;
 import bc1.gream.domain.order.repository.GifticonRepository;
+import bc1.gream.domain.order.repository.OrderRepository;
 import bc1.gream.domain.order.repository.SellRepository;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.repository.ProductRepository;
@@ -35,6 +42,10 @@ public class SellService {
     private final SellRepository sellRepository;
     private final ProductRepository productRepository;
     private final GifticonRepository gifticonRepository;
+    private final BuyRepository buyRepository;
+    private final OrderRepository orderRepository;
+
+    private final BuyService buyService;
 
     public SellBidResponseDto sellBidProduct(User user, SellBidRequestDto requestDto, Long productId) {
         Long price = requestDto.price();    // 판매하려는 가격
@@ -45,16 +56,17 @@ public class SellService {
         // 해당 date의 날짜의 가장 마지막 시간으로 세팅됩니다. 23시 59분 59초
         // 해당 date 기준으로 period만큼 날이 지난 날이 마감날로 세팅됩니다.
         Product product = getProductById(productId);
+        Gifticon gifticon = saveGifticon(requestDto.gifticonUrl(), null); // Order는 우선 null로 입력 추후 즉시 구매 시 Order 넣어줄 예정
 
         Sell sell = Sell.builder()
             .price(price)
             .deadlineAt(deadlineAt)
             .user(user)
             .product(product)
+            .gifticon(gifticon)
             .build();
 
         Sell savedSell = sellRepository.save(sell);
-        saveGifticon(requestDto.gifticonUrl(), savedSell, null); // Order는 우선 null로 입력 추후 즉시 구매 시 Order 넣어줄 예정
 
         return SellMapper.INSTANCE.toSellBidResponseDto(savedSell);
     }
@@ -63,15 +75,41 @@ public class SellService {
     public SellCancelBidResponseDto sellCancelBid(User user, Long sellId) {
         Sell bidSell = findSellById(sellId);
 
-        if (isNotSellerLoggedInUser(bidSell, user)) {
+        if (!isSellerLoggedInUser(bidSell, user)) {
             throw new GlobalException(NOT_AUTHORIZED);
         }
 
+        Long gifticonId = bidSell.getGifticon().getId();
+
         sellRepository.delete(bidSell);
-        deleteGifticon(sellId);
+        deleteGifticon(gifticonId);
 
         return new SellCancelBidResponseDto();
     }
+
+    public SellNowResponseDto sellNowProduct(User user, SellNowRequestDto requestDto, Long productId) {
+        Buy buy = buyRepository.findByProductIdAndPrice(productId, requestDto.price()).orElseThrow(
+            () -> new GlobalException(BUY_BID_PRODUCT_NOT_FOUND)
+        );
+        Long price = buy.getPrice();
+        User buyer = buy.getUser();
+        Long expectedPrice = buyService.calcDiscount(buy.getCouponId(), price, buyer);
+
+        Order order = Order.builder()
+            .product(buy.getProduct())
+            .buyer(buy.getUser())
+            .seller(user)
+            .finalPrice(price)
+            .expectedPrice(expectedPrice)
+            .build();
+
+        Order savedOrder = orderRepository.save(order);
+        saveGifticon(requestDto.gifticonUrl(), savedOrder);
+        buyRepository.delete(buy);
+
+        return OrderMapper.INSTANCE.toSellNowResponseDto(savedOrder);
+    }
+
 
     private Integer getPeriod(Integer period) {
         return Objects.requireNonNullElse(period, 7);
@@ -89,21 +127,20 @@ public class SellService {
         );
     }
 
-    private boolean isNotSellerLoggedInUser(Sell sell, User user) {
-        return !sell.getUser().getLoginId().equals(user.getLoginId());
+    private boolean isSellerLoggedInUser(Sell sell, User user) {
+        return sell.getUser().getLoginId().equals(user.getLoginId());
     }
 
-    private void saveGifticon(String gifticonUrl, Sell sell, Order order) {
+    private Gifticon saveGifticon(String gifticonUrl, Order order) {
         Gifticon gifticon = Gifticon.builder()
             .gifticonUrl(gifticonUrl)
-            .sell(sell)
             .order(order)
             .build();
-        gifticonRepository.save(gifticon);
+        return gifticonRepository.save(gifticon);
     }
 
-    private void deleteGifticon(Long sellId) {
-        Gifticon gifticon = gifticonRepository.findBySell_Id(sellId).orElseThrow(
+    private void deleteGifticon(Long gifticonId) {
+        Gifticon gifticon = gifticonRepository.findById(gifticonId).orElseThrow(
             () -> new GlobalException(GIFTICON_NOT_FOUND)
         );
 
