6a709d5900f8ad946c90a282f2ff9607738fe956
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-12 11:21:36 +0900

    test : 관심상품 등록 해제 통합테스트

--- src/test/java/bc1/gream/domain/product/controller/command/ProductLikeControllerTest.java
+++ src/test/java/bc1/gream/domain/product/controller/command/ProductLikeControllerTest.java
@@ -6,31 +6,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import bc1.gream.domain.product.controller.ProductLikeController;
 import bc1.gream.domain.product.service.command.ProductLikeService;
 import bc1.gream.global.security.WithMockCustomUser;
 import bc1.gream.test.BaseMvcTest;
 import bc1.gream.test.ProductTest;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@WebMvcTest(controllers = ProductLikeController.class)
+@WebMvcTest
 @WithMockCustomUser
+@ActiveProfiles("test")
 class ProductLikeControllerTest extends BaseMvcTest implements ProductTest {
 
     @Autowired
     WebApplicationContext context;
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
     @MockBean
     private ProductLikeService productLikeService;
 
@@ -47,7 +45,7 @@ class ProductLikeControllerTest extends BaseMvcTest implements ProductTest {
         productLikeService.likeProduct(TEST_USER, TEST_PRODUCT_ID);
 
         // THEN
-        this.mockMvc.perform(post("/api/products/" + TEST_PRODUCT_ID + "/like"))
+        this.mockMvc.perform(post("/api/products/{productId}/like", TEST_PRODUCT_ID))
             .andDo(print())
             .andExpect(status().isOk());
     }
@@ -58,7 +56,7 @@ class ProductLikeControllerTest extends BaseMvcTest implements ProductTest {
         productLikeService.dislikeProduct(TEST_USER, TEST_PRODUCT_ID);
 
         // THEN
-        this.mockMvc.perform(delete("/api/products/" + TEST_PRODUCT_ID + "/dislike"))
+        this.mockMvc.perform(delete("/api/products/{productId}/dislike", TEST_PRODUCT_ID))
             .andDo(print())
             .andExpect(status().isOk());
     }
