ad980ea1fc16c79ea48fd3b0148740c5d22433a5
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-13 11:32:27 +0900

    Revert "fix : ProductValidator 삭제"
    This reverts commit da3e4e5eb9a14d1d1e632c4cdce7acb3b9709a8d.

--- src/main/java/bc1/gream/domain/order/controller/SellNowController.java
+++ src/main/java/bc1/gream/domain/order/controller/SellNowController.java
@@ -3,6 +3,7 @@ package bc1.gream.domain.order.controller;
 import bc1.gream.domain.order.dto.request.SellNowRequestDto;
 import bc1.gream.domain.order.dto.response.SellNowResponseDto;
 import bc1.gream.domain.order.service.SellNowService;
+import bc1.gream.domain.order.validator.ProductValidator;
 import bc1.gream.global.common.RestResponse;
 import bc1.gream.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SellNowController {
 
     private final SellNowService sellNowService;
+    private final ProductValidator productValidator;
 
     @PostMapping("/{productId}/now")
     public RestResponse<SellNowResponseDto> sellNowProduct(
@@ -29,6 +31,7 @@ public class SellNowController {
         @PathVariable Long productId,
         @Valid @RequestBody SellNowRequestDto requestDto
     ) {
+        productValidator.validateBy(productId);
         SellNowResponseDto responseDto = sellNowService.sellNowProduct(userDetails.getUser(), requestDto, productId);
         return RestResponse.success(responseDto);
     }
