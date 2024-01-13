aaf1eab822633a48c1fb721ab22911e77801be8a
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-13 21:53:35 +0900

    fix : [REVIEW] Dealine @RequiredArgsConstructor 추가

--- src/main/java/bc1/gream/domain/order/service/helper/deadline/Deadline.java
+++ src/main/java/bc1/gream/domain/order/service/helper/deadline/Deadline.java
@@ -1,21 +1,19 @@
 package bc1.gream.domain.order.service.helper.deadline;
 
 import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public enum Deadline {
     WEEK(7);
 
     private final int period;
 
-    Deadline(int period) {
-        this.period = period;
-    }
-
     /**
      * 기간설정값이 있다면 해당값 반환, 없다면 WEEK 반환
      *
-     * @param period
-     * @return
+     * @param period 기간설정값
+     * @return 기간
      */
     public static Integer getPeriod(Integer period) {
         return Objects.requireNonNullElse(period, WEEK.period);
