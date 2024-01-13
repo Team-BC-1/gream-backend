29be28ec02c655234e788e05c7381605d26d1250
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-13 18:42:16 +0900

    fix : [REVIEW] CouponCalculator 연산  DiscountType에게 위임

--- src/main/java/bc1/gream/domain/order/service/helper/coupon/CouponCalculator.java
+++ src/main/java/bc1/gream/domain/order/service/helper/coupon/CouponCalculator.java
@@ -1,15 +1,12 @@
 package bc1.gream.domain.order.service.helper.coupon;
 
 import bc1.gream.domain.user.entity.Coupon;
-import bc1.gream.global.common.ResultCase;
-import bc1.gream.global.exception.GlobalException;
+import bc1.gream.domain.user.entity.DiscountType;
 
 public final class CouponCalculator {
 
     public static Long calculateDiscount(Coupon coupon, Long price) {
-        CouponCalculatorStrategy couponCalculatorStrategy = CouponCalculatorStrategyFactory
-            .getCouponCalculatorStrategy(coupon.getDiscountType())
-            .orElseThrow(() -> new GlobalException(ResultCase.COUPON_TYPE_NOT_FOUND));
-        return couponCalculatorStrategy.calculateDiscount(coupon, price);
+        DiscountType discountType = coupon.getDiscountType();
+        return discountType.calculateDiscount(coupon, price);
     }
 }
