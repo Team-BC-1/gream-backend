424b641d05d719945850f959a9f84a59dd7cf4ce
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-13 18:43:52 +0900

    fix : [REVIEW]  CouponRateCalculation 삭제

--- src/main/java/bc1/gream/domain/user/entity/DiscountType.java
+++ src/main/java/bc1/gream/domain/user/entity/DiscountType.java
@@ -1,6 +1,18 @@
 package bc1.gream.domain.user.entity;
 
 public enum DiscountType {
-    RATE,
-    FIX
+    RATE {
+        @Override
+        public Long calculateDiscount(Coupon coupon, Long price) {
+            return price * (100 - coupon.getDiscount()) / 100;
+        }
+    },
+    FIX {
+        @Override
+        public Long calculateDiscount(Coupon coupon, Long price) {
+            return price - coupon.getDiscount();
+        }
+    };
+
+    public abstract Long calculateDiscount(Coupon coupon, Long price);
 }
