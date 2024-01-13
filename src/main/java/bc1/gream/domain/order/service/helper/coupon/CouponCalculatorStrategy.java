8f96a961f039c3985c024fc98fc311821d7b85fc
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-13 18:41:24 +0900

    fix : [REVIEW] CouponCalculatorStrategy 삭제

--- src/main/java/bc1/gream/domain/order/service/helper/coupon/CouponCalculatorStrategy.java
+++
@@ -1,8 +0,0 @@
-package bc1.gream.domain.order.service.helper.coupon;
-
-import bc1.gream.domain.user.entity.Coupon;
-
-public interface CouponCalculatorStrategy {
-
-    Long calculateDiscount(Coupon coupon, Long price);
-}
