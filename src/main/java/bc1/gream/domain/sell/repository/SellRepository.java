b54aecbcd2162fd44003277054769f589ee01ec1
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 08:29:43 +0900

    move : Sell Repository 분리

--- src/main/java/bc1/gream/domain/sell/repository/SellRepository.java
+++ src/main/java/bc1/gream/domain/sell/repository/SellRepository.java
@@ -1,6 +1,6 @@
-package bc1.gream.domain.order.repository;
+package bc1.gream.domain.sell.repository;
 
-import bc1.gream.domain.order.entity.Sell;
+import bc1.gream.domain.sell.entity.Sell;
 import bc1.gream.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/bc1/gream/domain/sell/repository/SellRepositoryCustom.java
+++ src/main/java/bc1/gream/domain/sell/repository/SellRepositoryCustom.java
@@ -1,6 +1,6 @@
-package bc1.gream.domain.order.repository;
+package bc1.gream.domain.sell.repository;
 
-import bc1.gream.domain.order.entity.Sell;
+import bc1.gream.domain.sell.entity.Sell;
 import bc1.gream.domain.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
