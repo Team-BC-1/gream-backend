8d224b6fe53dc2a44e9aaa5c1e3ebd1b3680f86e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 08:08:42 +0900

    move : Buy RequestDto 분리

--- src/main/java/bc1/gream/domain/buy/dto/request/BuyCancelBidRequestDto.java
+++ src/main/java/bc1/gream/domain/buy/dto/request/BuyCancelBidRequestDto.java
@@ -1,4 +1,4 @@
-package bc1.gream.domain.order.dto.request;
+package bc1.gream.domain.buy.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
--- src/main/java/bc1/gream/domain/buy/dto/request/BuyNowRequestDto.java
+++ src/main/java/bc1/gream/domain/buy/dto/request/BuyNowRequestDto.java
@@ -1,13 +1,12 @@
-package bc1.gream.domain.order.dto.request;
+package bc1.gream.domain.buy.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record BuyBidRequestDto(
+public record BuyNowRequestDto(
     @NotNull(message = "가격 필드는 비울 수 없습니다.")
     Long price,
-    Integer period,
     Long couponId
 ) {
 
