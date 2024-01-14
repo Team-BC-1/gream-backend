b32f1b169632a52fce1c97a326d5f33288cee12d
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-14 16:29:27 +0900

    test : SellNowServiceIntegrationTest 즉시판매조회 동시에100개요청

---
+++ src/test/java/bc1/gream/domain/order/service/SellNowServiceIntegrationTest.java
@@ -0,0 +1,88 @@
+package bc1.gream.domain.order.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bc1.gream.domain.order.dto.request.SellNowRequestDto;
+import bc1.gream.domain.order.dto.response.SellNowResponseDto;
+import bc1.gream.domain.order.entity.Buy;
+import bc1.gream.domain.order.repository.BuyRepository;
+import bc1.gream.test.BaseIntegrationTest;
+import bc1.gream.test.BuyTest;
+import bc1.gream.test.CouponTest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class SellNowServiceIntegrationTest extends BaseIntegrationTest implements BuyTest, CouponTest {
+
+    @Autowired
+    private SellNowService sellNowService;
+    @Autowired
+    private BuyRepository buyRepository;
+
+    @BeforeEach
+    void setUp() {
+        setUpBaseIntegrationTest();
+        insertBuyByCount(200);  // 입찰이 200개 있다고 치자.
+    }
+
+    private void insertBuyByCount(int buyCount) {
+        for (int i = 0; i < buyCount; i++) {
+            Buy buy = Buy.builder()
+                .price(TEST_BUY_PRICE)
+                .deadlineAt(BuyTest.TEST_DEADLINE_AT)
+                .user(savedBuyer)
+                .product(savedProduct)
+                .couponId(savedCoupon.getId())
+                .build();
+            buyRepository.save(buy);
+        }
+    }
+
+    @Test
+    public void 즉시판매조회() {
+        // GIVEN
+        SellNowRequestDto requestDto = new SellNowRequestDto(TEST_BUY_PRICE, TEST_GIFTICON_URL);
+
+        // WHEN
+        SellNowResponseDto responseDto = sellNowService.sellNowProduct(savedSeller, requestDto, savedProduct.getId());
+
+        // THEN
+        System.out.println("responseDto = " + responseDto);
+    }
+
+    @Test
+    public void 즉시판매조회_동시에100개요청() {
+        // GIVEN
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        SellNowRequestDto requestDto = new SellNowRequestDto(TEST_BUY_PRICE, TEST_GIFTICON_URL);
+
+        // WHEN
+        List<SellNowResponseDto> sellNowResponseDtos = new ArrayList<>();
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                SellNowResponseDto responseDto = sellNowService.sellNowProduct(savedSeller, requestDto, savedProduct.getId());
+                sellNowResponseDtos.add(responseDto);
+//                try{
+//                } catch (InterruptedException ex) {
+//                    ex.printStackTrace();
+//                } finally {
+//                    countDownLatch.countDown();
+//                }
+            });
+        }
+
+        // THEN
+        List<SellNowResponseDto> uniqueResponseDtos = sellNowResponseDtos.stream()
+            .distinct()
+            .toList();
+        assertEquals(sellNowResponseDtos.size(), uniqueResponseDtos.size());
+    }
+}
