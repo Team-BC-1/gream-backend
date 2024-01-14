ba99831becb4e80b17ae64e2e08ad3ee6b2dcc53
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-14 17:13:58 +0900

    Revert "feat : OrderCommandService 쿠폰적용된 주문 발행 및 저장 시, 트랜잭션 사용하게끔 추가"
    This reverts commit 0f6f3f98a9a5ed36d9c72465a91f371f19039681.

--- src/main/java/bc1/gream/domain/order/service/command/OrderCommandService.java
+++ src/main/java/bc1/gream/domain/order/service/command/OrderCommandService.java
@@ -8,11 +8,9 @@ import bc1.gream.domain.user.entity.Coupon;
 import bc1.gream.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class OrderCommandService {
 
     private final OrderRepository orderRepository;
