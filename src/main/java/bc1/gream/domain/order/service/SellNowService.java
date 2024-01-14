b67a4a599503c3d18f34391530f58a539468bc8e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 08:11:39 +0900

    move : BuyService 분리

--- src/main/java/bc1/gream/domain/order/service/SellNowService.java
+++ src/main/java/bc1/gream/domain/order/service/SellNowService.java
@@ -1,11 +1,10 @@
 package bc1.gream.domain.order.service;
 
-import bc1.gream.domain.buy.entity.Buy;
-import bc1.gream.domain.buy.repository.BuyRepository;
 import bc1.gream.domain.buy.service.BuyService;
 import bc1.gream.domain.gifticon.service.GifticonService;
 import bc1.gream.domain.order.dto.request.SellNowRequestDto;
 import bc1.gream.domain.order.dto.response.SellNowResponseDto;
+import bc1.gream.domain.buy.entity.Buy;
 import bc1.gream.domain.order.entity.Order;
 import bc1.gream.domain.order.mapper.OrderMapper;
 import bc1.gream.domain.order.service.command.OrderCommandService;
@@ -21,7 +20,6 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class SellNowService {
 
-    private final BuyRepository buyRepository;
     private final BuyService buyService;
     private final CouponService couponService;
     private final OrderCommandService orderCommandService;
@@ -38,7 +36,7 @@ public class SellNowService {
         gifticonService.saveGifticon(requestDto.gifticonUrl(), order);
 
         // 구매입찰 삭제
-        buyRepository.delete(buy);
+        buyService.delete(buy);
 
         // 매퍼를 통해 변환
         return OrderMapper.INSTANCE.toSellNowResponseDto(order);
