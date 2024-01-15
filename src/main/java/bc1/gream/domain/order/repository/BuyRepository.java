35053ad442d18d590a56a2cf0e85324842040cd3
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 10:46:38 +0900

    fix : BuyRepository 불필요한 주석문 제거

--- src/main/java/bc1/gream/domain/order/repository/BuyRepository.java
+++ src/main/java/bc1/gream/domain/order/repository/BuyRepository.java
@@ -5,9 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuyRepository extends JpaRepository<Buy, Long>, BuyRepositoryCustom {
 
-//    @Query("select b from Buy b "
-//        + "where b.product.id = :productId and b.price = :price "
-//        + "order by b.createdAt desc limit 1")
-////        + "limit 1")
-//    Optional<Buy> findByProductIdAndPrice(@Param("productId") Long productId, @Param("price") Long price);
 }
