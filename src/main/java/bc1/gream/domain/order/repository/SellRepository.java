9c7ee66c4f1c1f67cc620cc43b61a2f86ca4890b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 09:20:55 +0900

    fix : SellRepository 가장 최근 Sell 조회 기능 수정

--- src/main/java/bc1/gream/domain/order/repository/SellRepository.java
+++ src/main/java/bc1/gream/domain/order/repository/SellRepository.java
@@ -4,15 +4,14 @@ import bc1.gream.domain.order.entity.Sell;
 import bc1.gream.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface SellRepository extends JpaRepository<Sell, Long>, SellRepositoryCustom {
 
-    @Query("select s from Sell s "
-        + "where s.price = :price and s.product.id = :productId "
-        + "order by s.createdAt asc "
-        + "limit 1")
-    Optional<Sell> findByProductIdAndPrice(Long productId, Long price);
+//    @Query(value = "select s from tb_sell s "
+//        + "where s.price = :price and s.product.id = :productId "
+//        + "order by s.createdAt desc "
+//        + "limit 1", nativeQuery = true)
+//    Optional<Sell> findByProductIdAndPrice(Long productId, Long price);
 
     Optional<Sell> findByIdAndUser(Long sellId, User user);
 }
---
+++ src/test/java/bc1/gream/domain/order/repository/SellRepositoryTest.java
@@ -0,0 +1,83 @@
+package bc1.gream.domain.order.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bc1.gream.domain.gifticon.repository.GifticonRepository;
+import bc1.gream.domain.order.entity.Gifticon;
+import bc1.gream.domain.order.entity.Sell;
+import bc1.gream.domain.product.entity.Product;
+import bc1.gream.domain.product.repository.ProductRepository;
+import bc1.gream.domain.user.entity.User;
+import bc1.gream.domain.user.repository.UserRepository;
+import bc1.gream.global.common.ResultCase;
+import bc1.gream.global.config.QueryDslConfig;
+import bc1.gream.global.exception.GlobalException;
+import bc1.gream.global.jpa.AuditingConfig;
+import bc1.gream.test.SellTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import({QueryDslConfig.class, AuditingConfig.class})
+class SellRepositoryTest implements SellTest {
+
+    @Autowired
+    GifticonRepository gifticonRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    private SellRepository sellRepository;
+    private Product savedProduct;
+    private User savedUser;
+
+    @BeforeEach
+    void setUp() {
+        savedProduct = productRepository.save(TEST_PRODUCT);
+        savedUser = userRepository.save(TEST_USER);
+    }
+
+    @Test
+    @DisplayName("상품과 가격에 대한 가장 최근 Sell 엔티티를 조회합니다.")
+    public void 가장최근Sell_상품가격_조건조회() {
+        // GIVEN
+        List<Sell> savedSells = new ArrayList<>();
+        int saveCount = 5;
+        for (int i = 0; i < saveCount; i++) {
+            Gifticon gifticon = gifticonRepository.save(
+                Gifticon.builder()
+                    .gifticonUrl(TEST_GIFTICON_URL)
+                    .build()
+            );
+            Sell save = sellRepository.save(
+                Sell.builder()
+                    .price(TEST_SELL_PRICE)
+                    .deadlineAt(TEST_DEADLINE_AT)
+                    .product(savedProduct)
+                    .user(savedUser)
+                    .gifticon(gifticon)
+                    .build()
+            );
+            savedSells.add(save);
+        }
+
+        // WHEN
+        Sell foundSell = sellRepository.findByProductIdAndPrice(TEST_PRODUCT_ID, TEST_SELL_PRICE)
+            .orElseThrow(() -> new GlobalException(ResultCase.SELL_BID_PRODUCT_NOT_FOUND));
+
+        // THEN
+        assertEquals(savedSells.get(savedSells.size() - 1), foundSell);
+    }
+}
