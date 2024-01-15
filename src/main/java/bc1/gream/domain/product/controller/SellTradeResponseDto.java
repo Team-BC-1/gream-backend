2b22f067ee3bc47474cb5b63028bf395c53784db
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 16:47:19 +0900

    feat : SellTradeResponseDto 에 @Builder 추가

--- src/main/java/bc1/gream/domain/product/controller/SellTradeResponseDto.java
+++ src/main/java/bc1/gream/domain/product/controller/SellTradeResponseDto.java
@@ -1,7 +1,9 @@
 package bc1.gream.domain.product.controller;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record SellTradeResponseDto(
     Long sellId,
     Long sellPrice,
