74031be06cfea2d32c7a3004ceb6260741c92259
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 16:35:40 +0900

    fix : 구매 입찰가 조회 반환 DTO 수정

---
+++ src/main/java/bc1/gream/domain/product/dto/response/BuyTradeResponseDto.java
@@ -0,0 +1,13 @@
+package bc1.gream.domain.product.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
+public record BuyTradeResponseDto(
+    Long buyId,
+    Long buyPrice,
+    LocalDateTime tradeDate
+) {
+
+}
