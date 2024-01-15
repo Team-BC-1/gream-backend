e106cf626b94f5c89c7954b084c0461bb17abe56
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 16:38:06 +0900

    feat : OrderTradeResponseDto @Builder 추가

--- src/main/java/bc1/gream/domain/product/dto/response/OrderTradeResponseDto.java
+++ src/main/java/bc1/gream/domain/product/dto/response/OrderTradeResponseDto.java
@@ -1,7 +1,9 @@
 package bc1.gream.domain.product.dto.response;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record OrderTradeResponseDto(
     Long id,
     Long finalPrice,
