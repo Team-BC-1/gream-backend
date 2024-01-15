4f212762de21ca0a93d071b2502e32c037ae1447
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-15 19:23:11 +0900

    fix : 구매입찰 조회 시, 최근 순서대로 조회 기능으로 변경

--- src/test/java/bc1/gream/domain/buy/repository/helper/BuyQueryOrderFactoryTest.java
+++ src/test/java/bc1/gream/domain/buy/repository/helper/BuyQueryOrderFactoryTest.java
@@ -1,15 +1,15 @@
-package bc1.gream.domain.sell.repository.helper;
+package bc1.gream.domain.buy.repository.helper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import bc1.gream.domain.sell.entity.QSell;
+import bc1.gream.domain.buy.entity.QBuy;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-class SellQueryOrderFactoryTest {
+class BuyQueryOrderFactoryTest {
 
     @Test
     public void 기본정렬기준생성() {
@@ -17,10 +17,10 @@ class SellQueryOrderFactoryTest {
         Pageable pageable = PageRequest.of(0, 10);
 
         // WHEN
-        OrderSpecifier[] orders = SellQueryOrderFactory.getOrdersOf(pageable.getSort());
+        OrderSpecifier[] orders = BuyQueryOrderFactory.getOrdersOf(pageable.getSort());
 
         // THEN
         assertEquals(Order.DESC, orders[0].getOrder());
-        assertEquals(QSell.sell.createdAt, orders[0].getTarget());
+        assertEquals(QBuy.buy.createdAt, orders[0].getTarget());
     }
 }
