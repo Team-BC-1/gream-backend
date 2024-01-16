c70c9d60db8f38f9528b263695b248742f753266
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-16 19:50:22 +0900

    [#129] feat : 유저의 포인트 조회 User entity point 추가 유저 생성시 point 100000추가 유저의 포인트 조회기능 추가

--- src/main/java/bc1/gream/domain/user/service/UserService.java
+++ src/main/java/bc1/gream/domain/user/service/UserService.java
@@ -1,9 +1,13 @@
 package bc1.gream.domain.user.service;
 
 import bc1.gream.domain.user.dto.request.UserSignupRequestDto;
+import bc1.gream.domain.user.dto.response.UserPointResponseDto;
 import bc1.gream.domain.user.dto.response.UserSignupResponseDto;
+import bc1.gream.global.security.UserDetailsImpl;
 
 public interface UserService {
 
     UserSignupResponseDto signup(UserSignupRequestDto request);
+
+    UserPointResponseDto points(UserDetailsImpl userDetails);
 }
--- src/main/java/bc1/gream/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/bc1/gream/domain/user/service/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package bc1.gream.domain.user.service.impl;
 
 import bc1.gream.domain.user.dto.request.UserSignupRequestDto;
+import bc1.gream.domain.user.dto.response.UserPointResponseDto;
 import bc1.gream.domain.user.dto.response.UserSignupResponseDto;
 import bc1.gream.domain.user.entity.Provider;
 import bc1.gream.domain.user.entity.User;
 import bc1.gream.domain.user.entity.UserRole;
+import bc1.gream.domain.user.mapper.UserMapper;
 import bc1.gream.domain.user.repository.UserRepository;
 import bc1.gream.domain.user.service.UserService;
 import bc1.gream.global.common.ResultCase;
 import bc1.gream.global.exception.GlobalException;
+import bc1.gream.global.security.UserDetailsImpl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,6 +41,12 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
 
         return new UserSignupResponseDto();
+    }
+
+    @Override
+    public UserPointResponseDto points(UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        return UserMapper.INSTANCE.toUserPointResponseDto(user);
     }
 
     private void validateExistingUser(UserSignupRequestDto request) {
