f33269c946781dc6a7219e37bd6a782d17d9e692
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-17 00:38:58 +0900

    docs : SellRepositoryCustomImpl javadoc 추가

--- src/main/java/bc1/gream/domain/sell/repository/SellRepositoryCustomImpl.java
+++ src/main/java/bc1/gream/domain/sell/repository/SellRepositoryCustomImpl.java
@@ -5,12 +5,13 @@ import static bc1.gream.domain.product.entity.QProduct.product;
 import static bc1.gream.domain.sell.entity.QSell.sell;
 import static bc1.gream.domain.user.entity.QUser.user;
 
+import bc1.gream.domain.product.dto.response.SellPriceToQuantityResponseDto;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.entity.QProduct;
-import bc1.gream.domain.sell.dto.response.SellPriceToQuantityResponseDto;
 import bc1.gream.domain.sell.entity.Sell;
 import bc1.gream.domain.sell.repository.helper.SellQueryOrderFactory;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +27,13 @@ public class SellRepositoryCustomImpl implements SellRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * 상품에 대한 전체 판매입찰 조건조회,페이징 처리
+     *
+     * @param product  상품
+     * @param pageable 페이징
+     * @return 판매입찰
+     */
     @Override
     public Page<Sell> findAllPricesOf(Product product, Pageable pageable) {
         // Get Orders By Columns
@@ -41,9 +49,23 @@ public class SellRepositoryCustomImpl implements SellRepositoryCustom {
             .limit(pageable.getPageSize())
             .fetch();
 
-        return PageableExecutionUtils.getPage(sells, pageable, sells::size);
+        JPAQuery<Long> countQuery = queryFactory
+            .select(sell.count())
+            .from(sell)
+            .leftJoin(sell.product, QProduct.product)
+            .where(sell.product.eq(product))
+            .orderBy(orderSpecifiers);
+
+        return PageableExecutionUtils.getPage(sells, pageable, countQuery::fetchOne);
     }
 
+    /**
+     * 상품 id와 가격에 대해 가장 나중에 생성된 판매입찰 반환
+     *
+     * @param productId 상품 id
+     * @param price     가격
+     * @return 가장 나중에 생성된 판매입찰 반환
+     */
     @Override
     public Optional<Sell> findByProductIdAndPrice(Long productId, Long price) {
         Sell foundSell = queryFactory.selectFrom(sell)
@@ -56,6 +78,13 @@ public class SellRepositoryCustomImpl implements SellRepositoryCustom {
         return Optional.ofNullable(foundSell);
     }
 
+    /**
+     * 상품에 대한 판매입찰가격수량에 대한 조건조회,페이징 처리. 기본정렬은 가격 오름차순
+     *
+     * @param product  상품
+     * @param pageable 페이징
+     * @return 판매입찰가격수량
+     */
     @Override
     public Page<SellPriceToQuantityResponseDto> findAllPriceToQuantityOf(Product product, Pageable pageable) {
         // Get Orders By Columns
