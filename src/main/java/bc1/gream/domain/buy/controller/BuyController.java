19843c5e1a28d282e15a0b3dc8a3a86a007be7c6
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-16 16:48:04 +0900

    [#120] refactor: BuyController 내에서 Bid와 Now 분리

--- src/main/java/bc1/gream/domain/buy/controller/BuyController.java
+++ src/main/java/bc1/gream/domain/buy/controller/BuyController.java
@@ -1,27 +1,8 @@
 package bc1.gream.domain.buy.controller;
 
 
-import static bc1.gream.domain.user.coupon.entity.CouponStatus.ALREADY_USED;
-import static bc1.gream.domain.user.coupon.entity.CouponStatus.AVAILABLE;
-import static bc1.gream.domain.user.coupon.entity.CouponStatus.IN_USE;
-
-import bc1.gream.domain.buy.dto.request.BuyBidRequestDto;
-import bc1.gream.domain.buy.dto.request.BuyNowRequestDto;
-import bc1.gream.domain.buy.dto.response.BuyBidResponseDto;
-import bc1.gream.domain.buy.dto.response.BuyCancelBidResponseDto;
-import bc1.gream.domain.buy.dto.response.BuyNowResponseDto;
-import bc1.gream.domain.buy.service.BuyService;
-import bc1.gream.domain.common.facade.ChangingCouponStatusFacade;
-import bc1.gream.global.common.RestResponse;
-import bc1.gream.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,47 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = "Bearer Authentication")
 public class BuyController {
 
-    private final BuyService buyService;
-    private final ChangingCouponStatusFacade changingCouponStatusFacade;
-
-    /**
-     * 구매입찰 생성
-     */
-    @PostMapping("/{productId}")
-    public RestResponse<BuyBidResponseDto> buyBidProduct(
-        @AuthenticationPrincipal UserDetailsImpl userDetails,
-        @Valid @RequestBody BuyBidRequestDto requestDto,
-        @PathVariable Long productId
-    ) {
-        changingCouponStatusFacade.changeCouponStatusByCouponId(requestDto.couponId(), userDetails.getUser(), IN_USE);
-        BuyBidResponseDto responseDto = buyService.buyBidProduct(userDetails.getUser(), requestDto, productId);
-        return RestResponse.success(responseDto);
-    }
-
-    /**
-     * 구매입찰 취소
-     */
-    @DeleteMapping("/bid/{buyId}")
-    public RestResponse<BuyCancelBidResponseDto> buyCancelBid(
-        @AuthenticationPrincipal UserDetailsImpl userDetails,
-        @PathVariable Long buyId
-    ) {
-        changingCouponStatusFacade.changeCouponStatus(buyId, userDetails.getUser(), AVAILABLE);
-        BuyCancelBidResponseDto responseDto = buyService.buyCancelBid(userDetails.getUser(), buyId);
-        return RestResponse.success(responseDto);
-    }
-
-    /**
-     * 즉시구매
-     */
-    @PostMapping("/{productId}/now")
-    public RestResponse<BuyNowResponseDto> buyNowProduct(
-        @AuthenticationPrincipal UserDetailsImpl userDetails,
-        @PathVariable Long productId,
-        @RequestBody BuyNowRequestDto requestDto
-    ) {
-        changingCouponStatusFacade.changeCouponStatusByCouponId(requestDto.couponId(), userDetails.getUser(), ALREADY_USED);
-        BuyNowResponseDto responseDto = buyService.buyNowProduct(userDetails.getUser(), requestDto, productId);
-        return RestResponse.success(responseDto);
-    }
 }
