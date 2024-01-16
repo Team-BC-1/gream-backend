1e4e5ba2843fbc33acd95b9268c18c4c107847b2
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-16 16:49:19 +0900

    [#120] refactor: BuyService에서 Now / Bid 분리

--- src/main/java/bc1/gream/domain/buy/service/BuyService.java
+++ src/main/java/bc1/gream/domain/buy/service/BuyService.java
@@ -1,33 +1,22 @@
 package bc1.gream.domain.buy.service;
 
 import static bc1.gream.global.common.ResultCase.BUY_BID_NOT_FOUND;
-import static bc1.gream.global.common.ResultCase.NOT_AUTHORIZED;
-import static bc1.gream.global.common.ResultCase.SELL_BID_PRODUCT_NOT_FOUND;
+import static bc1.gream.global.common.ResultCase.GIFTICON_NOT_FOUND;
 
-import bc1.gream.domain.buy.dto.request.BuyBidRequestDto;
-import bc1.gream.domain.buy.dto.request.BuyNowRequestDto;
-import bc1.gream.domain.buy.dto.response.BuyBidResponseDto;
-import bc1.gream.domain.buy.dto.response.BuyCancelBidResponseDto;
-import bc1.gream.domain.buy.dto.response.BuyNowResponseDto;
 import bc1.gream.domain.buy.entity.Buy;
-import bc1.gream.domain.buy.mapper.BuyMapper;
 import bc1.gream.domain.buy.repository.BuyRepository;
 import bc1.gream.domain.gifticon.repository.GifticonRepository;
+import bc1.gream.domain.order.entity.Gifticon;
 import bc1.gream.domain.order.entity.Order;
-import bc1.gream.domain.order.mapper.OrderMapper;
 import bc1.gream.domain.order.repository.OrderRepository;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.service.query.ProductService;
-import bc1.gream.domain.sell.entity.Sell;
 import bc1.gream.domain.sell.repository.SellRepository;
 import bc1.gream.domain.user.coupon.entity.Coupon;
 import bc1.gream.domain.user.coupon.entity.DiscountType;
 import bc1.gream.domain.user.entity.User;
 import bc1.gream.domain.user.service.CouponService;
 import bc1.gream.global.exception.GlobalException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -47,64 +36,8 @@ public class BuyService {
     private final OrderRepository orderRepository;
     private final GifticonRepository gifticonRepository;
 
-    public BuyBidResponseDto buyBidProduct(User user, BuyBidRequestDto requestDto, Long productId) {
-        Long price = requestDto.price();
-        Integer period = getPeriod(requestDto.period());
-        Long couponId = requestDto.couponId();
-        LocalDate date = LocalDate.now();
-        LocalDateTime deadlineAt = date.atTime(LocalTime.MAX).plusDays(period);
-        Product product = productService.findBy(productId);
 
-        Buy buy = Buy.builder()
-            .price(price)
-            .deadlineAt(deadlineAt)
-            .couponId(couponId)
-            .user(user)
-            .product(product)
-            .build();
-
-        Buy savedBuy = buyRepository.save(buy);
-
-        return BuyMapper.INSTANCE.toBuyBidResponseDto(savedBuy);
-    }
-
-
-    public BuyCancelBidResponseDto buyCancelBid(User user, Long buyId) {
-        Buy buyBid = findBuyById(buyId);
-
-        if (!isBuyerLoggedInUser(buyBid, user)) {
-            throw new GlobalException(NOT_AUTHORIZED);
-        }
-
-        buyRepository.delete(buyBid);
-
-        return new BuyCancelBidResponseDto(buyId);
-    }
-
-    public BuyNowResponseDto buyNowProduct(User user, BuyNowRequestDto requestDto, Long productId) {
-        Long price = requestDto.price();    // finalPrice
-        Sell sell = sellRepository.findByProductIdAndPrice(productId, price).orElseThrow(
-            () -> new GlobalException(SELL_BID_PRODUCT_NOT_FOUND)
-        );
-        Long expectedPrice = calcDiscount(requestDto.couponId(), price, user);
-
-        Order order = Order.builder()
-            .product(sell.getProduct())
-            .buyer(user)
-            .seller(sell.getUser())
-            .finalPrice(price)
-            .expectedPrice(expectedPrice)
-            .build();
-
-        sell.getGifticon().updateOrder(order);
-
-        Order savedOrder = orderRepository.save(order);
-        sellRepository.delete(sell);
-
-        return OrderMapper.INSTANCE.toBuyNowResponseDto(savedOrder);
-    }
-
-    private Integer getPeriod(Integer period) {
+    public Integer getPeriod(Integer period) {
         return Objects.requireNonNullElse(period, 7);
     }
 
@@ -114,7 +47,7 @@ public class BuyService {
         );
     }
 
-    private boolean isBuyerLoggedInUser(Buy buy, User user) {
+    public boolean isBuyerLoggedInUser(Buy buy, User user) {
         return buy.getUser().getLoginId().equals(user.getLoginId());
     }
 
@@ -128,6 +61,14 @@ public class BuyService {
             return price - coupon.getDiscount();
         }
         return price * (100 - coupon.getDiscount()) / 100;
+    }
+
+    public void orderGifticonBySellId(Long sellId, Order order) {
+        Gifticon gifticon = gifticonRepository.findBySell_Id(sellId).orElseThrow(
+            () -> new GlobalException(GIFTICON_NOT_FOUND)
+        );
+
+        gifticon.updateOrder(order);
     }
 
     /**
