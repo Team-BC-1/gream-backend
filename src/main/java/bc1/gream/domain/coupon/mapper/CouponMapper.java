94be1c8ac816f05c650d53d89976ecd14de66c71
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-16 20:55:24 +0900

    refactor : dto 네임 변경으로 인한 수정

--- src/main/java/bc1/gream/domain/coupon/mapper/CouponMapper.java
+++ src/main/java/bc1/gream/domain/coupon/mapper/CouponMapper.java
@@ -1,17 +1,17 @@
 package bc1.gream.domain.coupon.mapper;
 
-import bc1.gream.domain.coupon.dto.response.CouponListResponseDto;
-import bc1.gream.domain.coupon.dto.response.CouponUsedListResponseDto;
+import bc1.gream.domain.coupon.dto.response.CouponAvailableResponseDto;
+import bc1.gream.domain.coupon.dto.response.CouponUnavailableResponseDto;
 import bc1.gream.domain.coupon.entity.Coupon;
-import bc1.gream.domain.user.dto.response.UserPointResponseDto;
-import bc1.gream.domain.user.entity.User;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CouponMapper {
+
     CouponMapper INSTANCE = Mappers.getMapper(CouponMapper.class);
-    CouponListResponseDto toCouponListResponseDto(Coupon coupon);
-    CouponUsedListResponseDto toCouponUsedListResponseDto(Coupon coupon);
+
+    CouponAvailableResponseDto toCouponListResponseDto(Coupon coupon);
+
+    CouponUnavailableResponseDto toCouponUsedListResponseDto(Coupon coupon);
 }
