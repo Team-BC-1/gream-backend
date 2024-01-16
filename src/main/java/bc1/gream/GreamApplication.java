d6f73c0a4755963edecb818163ef6870ce97baff
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-17 05:49:26 +0900

    feat : main 에 @EnableScheduling 추가

--- src/main/java/bc1/gream/GreamApplication.java
+++ src/main/java/bc1/gream/GreamApplication.java
@@ -2,8 +2,10 @@ package bc1.gream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GreamApplication {
 
     public static void main(String[] args) {
