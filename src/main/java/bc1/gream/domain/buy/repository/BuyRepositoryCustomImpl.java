33de44bd032359743b65e9855d3b2672c319c482
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-17 18:42:04 +0900

    [#144] refactor: findAllBuyBidCoupon 기능 리팩토링

--- src/main/java/bc1/gream/domain/buy/repository/BuyRepositoryCustomImpl.java
+++ src/main/java/bc1/gream/domain/buy/repository/BuyRepositoryCustomImpl.java
@@ -6,7 +6,6 @@ import bc1.gream.domain.buy.entity.Buy;
 import bc1.gream.domain.buy.entity.QBuy;
 import bc1.gream.domain.buy.repository.helper.BuyQueryOrderFactory;
 import bc1.gream.domain.coupon.entity.QCoupon;
-import bc1.gream.domain.coupon.helper.CouponCalculator;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.entity.QProduct;
 import bc1.gream.domain.user.entity.User;
@@ -71,28 +70,17 @@ public class BuyRepositoryCustomImpl implements BuyRepositoryCustom {
             .where(QBuy.buy.user.eq(user))
             .fetch()
             .stream()
-            .map(tuple -> {
-                /** 매핑을 사용하고 싶었지만.. 혹시 방법이 있다면 알려주시면 감사하겠습니다..ㅠ
-                 현재 Buy에 coupon이 연관관계가 없어서 Join으로 가져와도 Buy Entity 자체에 Coupon에 대한 정보는
-                 Coupon의 id만 존재하는 상황입니다.. 현재 불가피하게 repository에서 로직을 처리했지만
-                 뭔가 다른 방법이 있지 않을까 고민하고 있는 중입니다...
-                 */
-                Long discountPrice = tuple.get(QBuy.buy.price);
-                if (tuple.get(QCoupon.coupon.id) != null) {
-                    discountPrice = CouponCalculator.calculateDiscount(tuple.get(QCoupon.coupon), tuple.get(QBuy.buy.price));
-                }
-
-                return BuyCheckBidResponseDto.builder()
-                    .buyId(tuple.get(QBuy.buy.id))
-                    .price(tuple.get(QBuy.buy.price))
-                    .productId(tuple.get(QBuy.buy.product.id))
-                    .productName(tuple.get(QBuy.buy.product.name))
-                    .productBrand(tuple.get(QBuy.buy.product.brand))
-                    .couponId(tuple.get(QCoupon.coupon.id))
-                    .couponName(tuple.get(QCoupon.coupon.name))
-                    .discountPrice(discountPrice)
-                    .build();
-            })
-            .toList();
+            .map(tuple -> BuyCheckBidResponseDto.builder()
+                .buyId(tuple.get(QBuy.buy.id))
+                .price(tuple.get(QBuy.buy.price))
+                .productId(tuple.get(QBuy.buy.product.id))
+                .productName(tuple.get(QBuy.buy.product.name))
+                .productBrand(tuple.get(QBuy.buy.product.brand))
+                .couponId(tuple.get(QCoupon.coupon.id))
+                .couponName(tuple.get(QCoupon.coupon.name))
+                .discountPrice(tuple.get(QBuy.buy.price))
+                .coupon(tuple.get(QCoupon.coupon))
+                .build()
+            ).toList();
     }
 }
