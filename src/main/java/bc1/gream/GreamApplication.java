087a5064e1a0a6b7c5f5494e09b7ca3c0add3e11
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-17 14:48:41 +0900

    feat : BatchConfig 생성

--- src/main/java/bc1/gream/GreamApplication.java
+++ src/main/java/bc1/gream/GreamApplication.java
@@ -2,10 +2,8 @@ package bc1.gream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class GreamApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/bc1/gream/global/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package bc1.gream.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class BatchConfig {
+
+}
