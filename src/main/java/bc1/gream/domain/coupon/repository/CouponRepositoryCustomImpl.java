9a7db4ed8aee00e2a002edd50596de5cb2670ab9
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-17 21:09:59 +0900

    fix : CouponRepositoryCustomImpl leftjoin 이슈 해결

--- src/main/java/bc1/gream/domain/coupon/repository/CouponRepositoryCustomImpl.java
+++ src/main/java/bc1/gream/domain/coupon/repository/CouponRepositoryCustomImpl.java
@@ -3,10 +3,9 @@ package bc1.gream.domain.coupon.repository;
 import static bc1.gream.domain.coupon.entity.CouponStatus.AVAILABLE;
 import static bc1.gream.domain.coupon.entity.CouponStatus.IN_USE;
 import static bc1.gream.domain.coupon.entity.QCoupon.coupon;
-import static bc1.gream.domain.product.entity.QProduct.product;
 
 import bc1.gream.domain.coupon.entity.Coupon;
-import bc1.gream.domain.product.repository.helper.ProductQueryConditionFactory;
+import bc1.gream.domain.user.entity.QUser;
 import bc1.gream.domain.user.entity.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -15,13 +14,15 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class CouponRepositoryCustomImpl implements CouponRepositoryCustom{
+public class CouponRepositoryCustomImpl implements CouponRepositoryCustom {
+
     private final JPAQueryFactory queryFactory;
 
     @Override
     public List<Coupon> availableCoupon(User user) {
         return queryFactory
             .selectFrom(coupon)
+            .leftJoin(coupon.user, QUser.user)
             .where(
                 coupon.user.eq(user),
                 coupon.status.eq(AVAILABLE)
@@ -33,6 +34,7 @@ public class CouponRepositoryCustomImpl implements CouponRepositoryCustom{
     public List<Coupon> unavailable(User user) {
         return queryFactory
             .selectFrom(coupon)
+            .leftJoin(coupon.user, QUser.user)
             .where(
                 coupon.user.eq(user),
                 coupon.status.eq(IN_USE)
