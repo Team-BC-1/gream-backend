913ca452d3d845d497d80a66f9477106529819f5
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-17 19:54:28 +0900

    feat : 상품 전체조회 시, 조건조회 가능하게끔 기능구현

--- src/main/java/bc1/gream/domain/product/controller/ProductQueryController.java
+++ src/main/java/bc1/gream/domain/product/controller/ProductQueryController.java
@@ -8,6 +8,7 @@ import bc1.gream.domain.product.dto.response.OrderTradeResponseDto;
 import bc1.gream.domain.product.dto.response.ProductDetailsResponseDto;
 import bc1.gream.domain.product.dto.response.ProductPreviewResponseDto;
 import bc1.gream.domain.product.dto.response.SellPriceToQuantityResponseDto;
+import bc1.gream.domain.product.dto.unit.ProductCondition;
 import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.mapper.ProductMapper;
 import bc1.gream.domain.product.provider.BuyOrderQueryProvider;
@@ -24,6 +25,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -43,8 +45,10 @@ public class ProductQueryController {
      * 상품 전체 조회
      */
     @GetMapping
-    public RestResponse<List<ProductPreviewResponseDto>> findAll() {
-        List<Product> products = productService.findAll();
+    public RestResponse<List<ProductPreviewResponseDto>> findAll(
+        @RequestParam ProductCondition productCondition
+    ) {
+        List<Product> products = productService.findAllBy(productCondition);
         List<ProductPreviewResponseDto> responseDtos = products.stream()
             .map(ProductMapper.INSTANCE::toPreviewResponseDto)
             .toList();
