4112e7b473f90dbfa78f3e99c5b8c65212149fa1
barami62 <wodbs7758@naver.com>
2024-01-18 19:57:22 +0900

    [#170] feat : 사용자의 관심상품 id 리스트 반환 메서드 추가

--- src/main/java/bc1/gream/domain/user/repository/UserRepository.java
+++ src/main/java/bc1/gream/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package bc1.gream.domain.user.repository;
 
 import bc1.gream.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -11,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByLoginId(String loginId);
 
     boolean existsByNickname(String nickname);
+
+    @Query("select lp.product.id from LikeProduct lp where lp.user = :user")
+    List<Long> findAllLikeProductIdByUser(User user);
 }
