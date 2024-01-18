b44cb4d18d3251c4eb352d44d85f063660271075
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-18 11:20:21 +0900

    [#161] fix: 쿠폰이 없을 경우 즉시 판매 분기 추가

--- src/main/java/bc1/gream/domain/sell/provider/SellNowProvider.java
+++ src/main/java/bc1/gream/domain/sell/provider/SellNowProvider.java
@@ -5,6 +5,7 @@ import bc1.gream.domain.buy.service.BuyService;
 import bc1.gream.domain.coupon.entity.Coupon;
 import bc1.gream.domain.coupon.entity.CouponStatus;
 import bc1.gream.domain.coupon.service.CouponService;
+import bc1.gream.domain.coupon.service.command.CouponCommandService;
 import bc1.gream.domain.gifticon.service.GifticonCommandService;
 import bc1.gream.domain.order.entity.Order;
 import bc1.gream.domain.order.mapper.OrderMapper;
@@ -25,15 +26,27 @@ public class SellNowProvider {
     private final CouponService couponService;
     private final OrderCommandService orderCommandService;
     private final GifticonCommandService gifticonCommandService;
+    private final CouponCommandService couponCommandService;
 
     @Transactional
     public SellNowResponseDto sellNowProduct(User user, SellNowRequestDto requestDto, Long productId) {
         // 해당상품과 가격에 대한 구매입찰
+
+        Order order;
+
         Buy buy = buyService.getRecentBuyBidOf(productId, requestDto.price());
         // 쿠폰 조회
         Coupon coupon = getCouponFrom(buy);
         // 새로운 주문
-        Order order = orderCommandService.saveOrderOfBuy(buy, user, coupon);
+        if (coupon != null) {
+            // 쿠폰이 존재할 때 쿠폰 상태 변경 및 쿠폰이 있는 order 저장 메소드로 이동
+            couponCommandService.changeCouponStatus(coupon, CouponStatus.ALREADY_USED);
+            order = orderCommandService.saveOrderOfBuy(buy, user, coupon);
+        } else {
+            // 쿠폰이 존재하지 않을 때 쿠폰이 없는 order 저장 메소드로 이동
+            order = orderCommandService.saveOrderOfBuyNotCoupon(buy, user);
+        }
+
         // 새로운 기프티콘 저장
         gifticonCommandService.saveGifticon(requestDto.gifticonUrl(), order);
         // 판매에 따른 사용자 포인트 충전
