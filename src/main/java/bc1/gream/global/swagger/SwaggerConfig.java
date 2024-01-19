65c06430f3409d9e798b094d155d820d332da042
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-19 17:16:55 +0900

    refactor: Swagger Config 수정 (Access-Token과 Refresh-Token 두 가지 입력 할 수 있게 변경)

--- src/main/java/bc1/gream/global/swagger/SwaggerConfig.java
+++ src/main/java/bc1/gream/global/swagger/SwaggerConfig.java
@@ -1,11 +1,13 @@
 package bc1.gream.global.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,18 +15,38 @@ import org.springframework.context.annotation.Configuration;
     info = @Info(title = "Gream API DOCS",
         description = "Gream's API could give you a real-time bidding data of .",
         version = "v1"))
-@SecurityScheme(
-    name = "Bearer Authentication",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    scheme = "bearer"
-)
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String accessToken = "Access-Token";
+        String refreshToken = "refresh-Token";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList(accessToken)
+            .addList(refreshToken);
+
+        SecurityScheme accessTokenScheme = new SecurityScheme()
+            .type(Type.APIKEY)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(In.HEADER)
+            .name("Access-Token");
+
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+            .type(Type.APIKEY)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(In.HEADER)
+            .name("Refresh-Token");
+
+        Components components = new Components()
+            .addSecuritySchemes(accessToken, accessTokenScheme)
+            .addSecuritySchemes(refreshToken, refreshTokenScheme);
+
         return new OpenAPI()
-            .components(new Components());
+            .addSecurityItem(securityRequirement)
+            .components(components);
     }
 }
