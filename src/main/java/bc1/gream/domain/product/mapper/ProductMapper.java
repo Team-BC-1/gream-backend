b9c183e4387cd4f8283cff4aa02272bfc4b0f1ee
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-21 04:25:12 +0900

    refactor : ProductMapper 각각의 메소드에 @Mapping 추가

--- src/main/java/bc1/gream/domain/product/mapper/ProductMapper.java
+++ src/main/java/bc1/gream/domain/product/mapper/ProductMapper.java
@@ -8,6 +8,7 @@ import bc1.gream.domain.product.dto.response.ProductPreviewByNameResponseDto;
 import bc1.gream.domain.product.dto.response.ProductPreviewResponseDto;
 import bc1.gream.domain.product.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -16,15 +17,42 @@ public interface ProductMapper {
 
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
+    @Mapping(source = "id", target = "productId")
+    @Mapping(source = "brand", target = "productBrand")
+    @Mapping(source = "name", target = "productName")
+    @Mapping(source = "imageUrl", target = "productImageUrl")
+    @Mapping(source = "description", target = "productDescription")
+    @Mapping(source = "price", target = "productPrice")
     ProductDetailsResponseDto toDetailsResponseDto(Product product);
 
     ProductLikeResponseDto toLikeResponseDto(String message);
 
     ProductDislikeResponseDto toDislikeResponseDto(String message);
 
+
+    @Mapping(source = "id", target = "productId")
+    @Mapping(source = "brand", target = "productBrand")
+    @Mapping(source = "name", target = "productName")
+    @Mapping(source = "imageUrl", target = "productImageUrl")
+    @Mapping(source = "description", target = "productDescription")
+    @Mapping(source = "price", target = "productPrice")
     ProductPreviewResponseDto toPreviewResponseDto(Product product);
 
+
+    @Mapping(source = "id", target = "productId")
+    @Mapping(source = "brand", target = "productBrand")
+    @Mapping(source = "name", target = "productName")
+    @Mapping(source = "imageUrl", target = "productImageUrl")
+    @Mapping(source = "description", target = "productDescription")
+    @Mapping(source = "price", target = "productPrice")
     ProductLikesResponseDto toProductLikesResponseDto(Product product);
 
+
+    @Mapping(source = "id", target = "productId")
+    @Mapping(source = "brand", target = "productBrand")
+    @Mapping(source = "name", target = "productName")
+    @Mapping(source = "imageUrl", target = "productImageUrl")
+    @Mapping(source = "description", target = "productDescription")
+    @Mapping(source = "price", target = "productPrice")
     ProductPreviewByNameResponseDto toProductPreviewByNameResponseDto(Product product);
 }
