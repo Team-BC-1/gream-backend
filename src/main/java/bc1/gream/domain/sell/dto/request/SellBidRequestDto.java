11de0b3174cc05602122fc084a5f7248638beed1
barami62 <wodbs7758@naver.com>
2024-01-20 21:47:47 +0900

    [#180] fix : 요청 DTO를 multipart file 로 받도록 수정

--- src/main/java/bc1/gream/domain/sell/dto/request/SellBidRequestDto.java
+++ src/main/java/bc1/gream/domain/sell/dto/request/SellBidRequestDto.java
@@ -1,16 +1,16 @@
 package bc1.gream.domain.sell.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record SellBidRequestDto(
     @NotNull(message = "가격 필드는 비울 수 없습니다.")
     Long price,
     Integer period,
-    @NotBlank(message = "기프티콘을 업로드해 주세요")
-    String gifticonUrl
+    @NotNull(message = "기프티콘을 업로드해 주세요")
+    MultipartFile file
 ) {
 
 }
--- src/main/java/bc1/gream/domain/sell/dto/request/SellNowRequestDto.java
+++ src/main/java/bc1/gream/domain/sell/dto/request/SellNowRequestDto.java
@@ -1,13 +1,13 @@
 package bc1.gream.domain.sell.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
 
 public record SellNowRequestDto(
     @NotNull(message = "가격 필드는 비울 수 없습니다.")
     Long price,
-    @NotBlank(message = "기프티콘을 업로드해 주세요")
-    String gifticonUrl
+    @NotNull(message = "기프티콘을 업로드해 주세요")
+    MultipartFile file
 ) {
 
 }
