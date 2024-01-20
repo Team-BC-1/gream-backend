f6e424649e1d6ef9d8c2d1594a15a150451220a8
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-21 05:04:15 +0900

    refactor : CouponMapper @Mapping 추가

--- src/main/java/bc1/gream/domain/coupon/mapper/CouponMapper.java
+++ src/main/java/bc1/gream/domain/coupon/mapper/CouponMapper.java
@@ -4,6 +4,7 @@ import bc1.gream.domain.coupon.dto.response.CouponAvailableResponseDto;
 import bc1.gream.domain.coupon.dto.response.CouponUnavailableResponseDto;
 import bc1.gream.domain.coupon.entity.Coupon;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,7 +12,16 @@ public interface CouponMapper {
 
     CouponMapper INSTANCE = Mappers.getMapper(CouponMapper.class);
 
+    @Mapping(source = "id", target = "couponId")
+    @Mapping(source = "name", target = "couponName")
+    @Mapping(source = "discountType", target = "couponDiscountType")
+    @Mapping(source = "discount", target = "couponDiscount")
     CouponAvailableResponseDto toCouponListResponseDto(Coupon coupon);
 
+
+    @Mapping(source = "id", target = "couponId")
+    @Mapping(source = "name", target = "couponName")
+    @Mapping(source = "discountType", target = "couponDiscountType")
+    @Mapping(source = "discount", target = "couponDiscount")
     CouponUnavailableResponseDto toCouponUsedListResponseDto(Coupon coupon);
 }
