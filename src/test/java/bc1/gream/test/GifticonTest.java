b989467f13ce3a74e8b604e6209335676d991959
barami62 <wodbs7758@naver.com>
2024-01-20 23:28:26 +0900

    [#180] feat : 기프티콘 테스트 필드 추가

--- src/test/java/bc1/gream/test/GifticonTest.java
+++ src/test/java/bc1/gream/test/GifticonTest.java
@@ -1,12 +1,16 @@
 package bc1.gream.test;
 
 import bc1.gream.domain.gifticon.entity.Gifticon;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface GifticonTest extends OrderTest {
 
     Long TEST_GIFTICON_ID = 1L;
 
     String TEST_GIFTICON_URL = "images/images.png";
+    MultipartFile TEST_GIFTICON_FILE = new MockMultipartFile("file", "image.png", "image/png", "content".getBytes());
+    String TEST_S3_IMAGE_URL = "https://cataas.com/cat";
 
     Gifticon TEST_GIFTICON_END = Gifticon.builder()
         .gifticonUrl(TEST_GIFTICON_URL)
