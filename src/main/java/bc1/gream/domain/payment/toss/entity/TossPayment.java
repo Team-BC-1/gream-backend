7e92de36bc7681420edc0f56cd3f462e17e030ee
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-22 22:43:36 +0900

    feat : TossPayment 엔티티 구현

---
+++ src/main/java/bc1/gream/domain/payment/toss/entity/TossPayment.java
@@ -0,0 +1,50 @@
+package bc1.gream.domain.payment.toss.entity;
+
+import bc1.gream.domain.common.model.BaseEntity;
+import bc1.gream.domain.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "toss_payment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TossPayment extends BaseEntity {
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user")
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private PayType payType;
+
+    @Column
+    private Long amount;
+
+    @Column
+    private Long orderId;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private OrderName orderName;
+}
