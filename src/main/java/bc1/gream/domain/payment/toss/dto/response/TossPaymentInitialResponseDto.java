9e3570ba52d64efbd5d9cf6dd1b2636785c39a89
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-23 02:47:31 +0900

    feat : TossPaymentInitialResponseDto paymentCreatedAt 타입 LocalDateTime으로 변경

--- src/main/java/bc1/gream/domain/payment/toss/dto/response/TossPaymentInitialResponseDto.java
+++ src/main/java/bc1/gream/domain/payment/toss/dto/response/TossPaymentInitialResponseDto.java
@@ -2,6 +2,7 @@ package bc1.gream.domain.payment.toss.dto.response;
 
 import bc1.gream.domain.payment.toss.entity.OrderName;
 import bc1.gream.domain.payment.toss.entity.PayType;
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 /* 결제요청 시, 필요한 값 */
@@ -15,7 +16,7 @@ public record TossPaymentInitialResponseDto(
     String userNickname,               // 구매자 닉네임
     String paymentSuccessUrl,          // 주문 성공 시 콜백 주소
     String paymentFailUrl,             // 주문 실패 시 콜백 주소
-    String paymentCreatedAt,           // 결제날짜
+    LocalDateTime paymentCreatedAt,           // 결제날짜
     Boolean paymentHasSuccess          // 결제 성공 여부
 ) {
 
