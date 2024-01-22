9611aaee49621af195c00da51ffea22596e4be0f
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-23 01:03:50 +0900

    feat : TossPaymentRepository orderId를 통한 조회함수 구현

--- src/main/java/bc1/gream/domain/payment/toss/repository/TossPaymentRepository.java
+++ src/main/java/bc1/gream/domain/payment/toss/repository/TossPaymentRepository.java
@@ -1,8 +1,10 @@
 package bc1.gream.domain.payment.toss.repository;
 
 import bc1.gream.domain.payment.toss.entity.TossPayment;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TossPaymentRepository extends JpaRepository<TossPayment, Long> {
 
+    Optional<TossPayment> findByOrderId(Long orderId);
 }
