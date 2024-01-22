9dffaea93f02de8fffaafc93c94f27cd54b3870f
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-23 00:17:27 +0900

    feat : TossPaymentInitialResponseDto 내부 필드 payment prefix 추가

--- src/main/java/bc1/gream/domain/payment/toss/dto/response/TossPaymentInitialResponseDto.java
+++ src/main/java/bc1/gream/domain/payment/toss/dto/response/TossPaymentInitialResponseDto.java
@@ -7,16 +7,16 @@ import lombok.Builder;
 /* 결제요청 시, 필요한 값 */
 @Builder
 public record TossPaymentInitialResponseDto(
-    PayType payType,            // 결제방법
-    Long amount,                // 결제금액
-    Long orderId,               // 주문Id
-    OrderName orderName,        // 주문명
-    String userLoginId,         // 구매자 아이디
-    String userNickname,        // 구매자 닉네임
-    String paymentSuccessUrl,   // 주문 성공 시 콜백 주소
-    String paymentFailUrl,      // 주문 실패 시 콜백 주소
-    String paymentCreatedAt,    // 결제날짜
-    Boolean paymentHasSuccess   // 결제 성공 여부
+    PayType paymentPayType,            // 결제방법
+    Long paymentAmount,                // 결제금액
+    Long paymentOrderId,               // 주문Id
+    OrderName paymentOrderName,        // 주문명
+    String userLoginId,                // 구매자 아이디
+    String userNickname,               // 구매자 닉네임
+    String paymentSuccessUrl,          // 주문 성공 시 콜백 주소
+    String paymentFailUrl,             // 주문 실패 시 콜백 주소
+    String paymentCreatedAt,           // 결제날짜
+    Boolean paymentHasSuccess          // 결제 성공 여부
 ) {
 
 }
