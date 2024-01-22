bd5f226fc310bed0b266640937983022a754ebff
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-23 01:23:19 +0900

    feat : TossPaymentService 토스페이 결제 실패 리다이렉트 핸들러 구현

--- src/main/java/bc1/gream/domain/payment/toss/service/TossPaymentService.java
+++ src/main/java/bc1/gream/domain/payment/toss/service/TossPaymentService.java
@@ -1,5 +1,6 @@
 package bc1.gream.domain.payment.toss.service;
 
+import bc1.gream.domain.payment.toss.dto.response.TossPaymentFailResponseDto;
 import bc1.gream.domain.payment.toss.dto.response.TossPaymentInitialResponseDto;
 import bc1.gream.domain.payment.toss.dto.response.TossPaymentSuccessResponseDto;
 import bc1.gream.domain.payment.toss.entity.TossPayment;
@@ -35,18 +36,75 @@ public class TossPaymentService {
     @Value("${payment.toss.fail_url}")
     private String failUrl;
 
+    /**
+     * 토스페이 초기 요청 핸들러
+     *
+     * @param tossPayment 토스페이
+     * @return 토스페이 초기요청 결과
+     */
     @Transactional
     public TossPaymentInitialResponseDto requestTossPayment(TossPayment tossPayment) {
         TossPayment savedTossPayment = tossPaymentRepository.save(tossPayment);
         return TossPaymentMapper.INSTANCE.fromTossPaymentInitialResponseDto(savedTossPayment, successUrl, failUrl);
     }
 
+    /**
+     * 토스페이 최종 요청 핸들러
+     *
+     * @param paymentKey 토스 결제고유번호
+     * @param orderId    서버 주문고유번호
+     * @param amount     결제액
+     * @return 토스페이 최종요청 결과
+     */
     @Transactional
     public TossPaymentSuccessResponseDto requestFinalTossPayment(String paymentKey, Long orderId, Long amount) {
         this.verifyRequest(paymentKey, orderId, amount);
         return this.sendFinalRequestToTossApi(paymentKey, orderId, amount);
     }
 
+    /**
+     * 토스페이 실패 요청 핸들러
+     *
+     * @param errorCode 에러 코드
+     * @param errorMsg  에러 메세지
+     * @param orderId   서버 주문고유번호
+     * @return 결제실패정보
+     */
+    @Transactional
+    public TossPaymentFailResponseDto requestFail(String errorCode, String errorMsg, Long orderId) {
+        TossPayment tossPayment = this.findBy(orderId);
+        tossPayment.setIsPaySuccess(false);
+        tossPayment.setPayFailReason(errorMsg);
+        return TossPaymentMapper.INSTANCE.toTossPaymentFailResponseDto(errorCode, errorCode, orderId);
+    }
+
+    /**
+     * 주문아이디와 결제액에 대한 검증 핸들러
+     *
+     * @param paymentKey 토스 결제고유번호
+     * @param orderId    서버 주문고유번호
+     * @param amount     결제액
+     */
+    @Transactional
+    void verifyRequest(String paymentKey, Long orderId, Long amount) {
+        // 주문아이디 일치 검증
+        TossPayment tossPayment = findBy(orderId);
+        // 결제금액 일치 검증
+        if (tossPayment.getAmount().equals(amount)) {
+            tossPayment.setPaymentKey(paymentKey);
+            return;
+        }
+        throw new GlobalException(ResultCase.UNMATCHED_PAYMENT_AMOUNT);
+    }
+
+    /**
+     * 토스페이API에 POST요청
+     *
+     * @param paymentKey 토스 결제고유번호
+     * @param orderId    서버 주문고유번호
+     * @param amount     결제액
+     * @return 토스페이API 요청결과
+     */
     @Transactional
     TossPaymentSuccessResponseDto sendFinalRequestToTossApi(String paymentKey, Long orderId, Long amount) {
         RestTemplate rest = new RestTemplate();
@@ -70,16 +128,9 @@ public class TossPaymentService {
         );
     }
 
-    @Transactional
-    void verifyRequest(String paymentKey, Long orderId, Long amount) {
-        // 주문아이디 일치 검증
-        TossPayment tossPayment = tossPaymentRepository.findByOrderId(orderId)
+    @Transactional(readOnly = true)
+    TossPayment findBy(Long orderId) {
+        return tossPaymentRepository.findByOrderId(orderId)
             .orElseThrow(() -> new GlobalException(ResultCase.PAYMENT_NOT_FOUND));
-        // 결제금액 일치 검증
-        if (tossPayment.getAmount().equals(amount)) {
-            tossPayment.setPaymentKey(paymentKey);
-            return;
-        }
-        throw new GlobalException(ResultCase.UNMATCHED_PAYMENT_AMOUNT);
     }
 }
