c56e7013fb0cd9210321d8d56aff33e606b5682c
barami62 <wodbs7758@naver.com>
2024-01-26 20:41:23 +0900

    [#199] feat : RefundRepository 추가

---
+++ src/main/java/bc1/gream/domain/user/repository/RefundRepository.java
@@ -0,0 +1,8 @@
+package bc1.gream.domain.user.repository;
+
+import bc1.gream.domain.user.entity.Refund;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefundRepository extends JpaRepository<Refund, Long> {
+
+}
