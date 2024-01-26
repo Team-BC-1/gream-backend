be06926ffb18da52a46e93d1f02fee09ce1f5830
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-26 21:16:31 +0900

    feat : 이름으로 상품 찾기 생성

--- src/main/java/bc1/gream/domain/product/repository/ProductRepository.java
+++ src/main/java/bc1/gream/domain/product/repository/ProductRepository.java
@@ -2,9 +2,12 @@ package bc1.gream.domain.product.repository;
 
 import bc1.gream.domain.product.entity.Product;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
     List<Product> findAllByNameContaining(String name);
+
+    Optional<Product> findFirstByName(String name);
 }
