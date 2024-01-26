b838ff6b0ef63c18aa707e4b679ae237e35e062d
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-26 19:29:11 +0900

    feat : admin controller 생성

---
+++ src/main/java/bc1/gream/domain/admin/controller/AdminController.java
@@ -0,0 +1,12 @@
+package bc1.gream.domain.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+
+}
