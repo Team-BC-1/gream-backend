358844809957d822221b7c4bea1883f1b0c91e6b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-26 14:34:58 +0900

    refactor : Gifticon 에서 Order FK 제거

--- src/main/java/bc1/gream/domain/gifticon/entity/Gifticon.java
+++ src/main/java/bc1/gream/domain/gifticon/entity/Gifticon.java
@@ -1,16 +1,12 @@
 package bc1.gream.domain.gifticon.entity;
 
 import bc1.gream.domain.common.model.BaseEntity;
-import bc1.gream.domain.order.entity.Order;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -31,17 +27,8 @@ public class Gifticon extends BaseEntity {
     @Column(name = "gifticon_url")
     private String gifticonUrl;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "order_id", unique = true)
-    private Order order;
-
     @Builder
-    private Gifticon(String gifticonUrl, Order order) {
+    private Gifticon(String gifticonUrl) {
         this.gifticonUrl = gifticonUrl;
-        this.order = order;
-    }
-
-    public void updateOrder(Order order) {
-        this.order = order;
     }
 }
