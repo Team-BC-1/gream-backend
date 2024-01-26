a8b6dfc4202bdd2a35798665357514fc777812de
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2024-01-26 21:19:34 +0900

    feat : 관리자 상품 controller 생성

--- src/main/java/bc1/gream/domain/admin/controller/AdminController.java
+++ src/main/java/bc1/gream/domain/admin/controller/AdminController.java
@@ -1,6 +1,12 @@
 package bc1.gream.domain.admin.controller;
 
+import bc1.gream.domain.admin.dto.AdminProductRequestDto;
+import bc1.gream.domain.admin.dto.AdminProductResponseDto;
+import bc1.gream.domain.product.service.query.ProductService;
+import bc1.gream.global.common.RestResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public RestResponse<AdminProductResponseDto> addProducts(
+        @RequestBody AdminProductRequestDto adminProductRequestDto
+    ) {
+        productService.addProducts(adminProductRequestDto);
+        return RestResponse.success(new AdminProductResponseDto());
+    }
 }
