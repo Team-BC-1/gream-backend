f6067994aa28b17ce33bb6d83f39a984ef4fefa1
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-28 06:24:19 +0900

    feat : DiscountType 역직렬화 함수 구현

--- src/main/java/bc1/gream/domain/coupon/entity/DiscountType.java
+++ src/main/java/bc1/gream/domain/coupon/entity/DiscountType.java
@@ -2,6 +2,8 @@ package bc1.gream.domain.coupon.entity;
 
 import bc1.gream.global.common.ResultCase;
 import bc1.gream.global.exception.GlobalException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.stream.Stream;
 
 public enum DiscountType {
     RATE {
@@ -22,6 +24,14 @@ public enum DiscountType {
             return price - coupon.getDiscount();
         }
     };
+
+    @JsonCreator
+    public static DiscountType deserializeRequest(String discountTypeRequest) {
+        return Stream.of(DiscountType.values())
+            .filter(discountType -> discountType.toString().equals(discountTypeRequest.toUpperCase()))
+            .findAny()
+            .orElseThrow(() -> new GlobalException(ResultCase.INVALID_INPUT));
+    }
 
     public abstract Long calculateDiscount(Coupon coupon, Long price);
 }
