db8f68021c731afdb2008732c1b5ec046b99aa5d
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-28 06:24:04 +0900

    feat : CouponStatus 역직렬화 함수 구현

--- src/main/java/bc1/gream/domain/coupon/entity/CouponStatus.java
+++ src/main/java/bc1/gream/domain/coupon/entity/CouponStatus.java
@@ -1,7 +1,20 @@
 package bc1.gream.domain.coupon.entity;
 
+import bc1.gream.global.common.ResultCase;
+import bc1.gream.global.exception.GlobalException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.stream.Stream;
+
 public enum CouponStatus {
     AVAILABLE,
     IN_USE,
-    ALREADY_USED
+    ALREADY_USED;
+
+    @JsonCreator
+    public static CouponStatus deserializeRequest(String couponStatusRequest) {
+        return Stream.of(CouponStatus.values())
+            .filter(couponStatus -> couponStatus.toString().equals(couponStatusRequest.toUpperCase()))
+            .findAny()
+            .orElseThrow(() -> new GlobalException(ResultCase.INVALID_INPUT));
+    }
 }
