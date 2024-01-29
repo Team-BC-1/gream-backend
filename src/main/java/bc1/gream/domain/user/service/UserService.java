dd5e1dd14a2a07f1b0ef34a05118f0c6436aa3db
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-29 20:35:34 +0900

    [#207]feat: 유저 포인트 환급 신청 서비스 api 추가

--- src/main/java/bc1/gream/domain/user/service/UserService.java
+++ src/main/java/bc1/gream/domain/user/service/UserService.java
@@ -1,12 +1,17 @@
 package bc1.gream.domain.user.service;
 
+import bc1.gream.domain.user.dto.request.UserPointRefundRequestDto;
 import bc1.gream.domain.user.dto.request.UserSignupRequestDto;
+import bc1.gream.domain.user.dto.response.UserPointRefundResponseDto;
 import bc1.gream.domain.user.dto.response.UserPointResponseDto;
 import bc1.gream.domain.user.dto.response.UserSignupResponseDto;
 import bc1.gream.domain.user.entity.Provider;
+import bc1.gream.domain.user.entity.Refund;
 import bc1.gream.domain.user.entity.User;
 import bc1.gream.domain.user.entity.UserRole;
+import bc1.gream.domain.user.mapper.RefundMapper;
 import bc1.gream.domain.user.mapper.UserMapper;
+import bc1.gream.domain.user.repository.RefundRepository;
 import bc1.gream.domain.user.repository.UserRepository;
 import bc1.gream.global.common.ResultCase;
 import bc1.gream.global.exception.GlobalException;
@@ -21,6 +26,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RefundRepository refundRepository;
     private final PasswordEncoder passwordEncoder;
 
 
@@ -55,5 +61,34 @@ public class UserService {
         if (userRepository.existsByNickname(request.nickname())) {
             throw new GlobalException(ResultCase.DUPLICATED_NICKNAME);
         }
+    }
+
+    public User findUser(String loginId) {
+        User user = userRepository.findByLoginId(loginId).orElseThrow(
+            () -> new GlobalException(ResultCase.USER_NOT_FOUND)
+        );
+
+        return user;
+    }
+
+    public UserPointRefundResponseDto refundsPoint(User user, UserPointRefundRequestDto requestDto) {
+        User findUser = findUser(user.getLoginId());
+
+        Refund refund = Refund.builder()
+            .point(requestDto.point())
+            .bank(requestDto.bank())
+            .accountNumber(requestDto.accountNumber())
+            .user(findUser)
+            .build();
+
+        Refund savedRefund = refundRepository.save(refund);
+
+        return RefundMapper.INSTANCE.toUserPointRefundResponseDto(savedRefund);
+    }
+
+    public Refund findRefund(Long refundId) {
+        return refundRepository.findById(refundId).orElseThrow(
+            () -> new GlobalException(ResultCase.REFUND_NOT_FOUND)
+        );
     }
 }
