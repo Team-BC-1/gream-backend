d9d9ee2808e524990db4c93ca6dda68fea7176db
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-30 12:59:33 +0900

    feat : CouponProvider 를 통해 쿠폰 생성 처리
    - User 와 CouponCommandService 사용

--- src/main/java/bc1/gream/domain/admin/controller/AdminController.java
+++ src/main/java/bc1/gream/domain/admin/controller/AdminController.java
@@ -4,7 +4,7 @@ import bc1.gream.domain.admin.dto.request.AdminCreateCouponRequestDto;
 import bc1.gream.domain.admin.dto.response.AdminCreateCouponResponseDto;
 import bc1.gream.domain.coupon.entity.Coupon;
 import bc1.gream.domain.coupon.mapper.CouponMapper;
-import bc1.gream.domain.coupon.service.command.CouponCommandService;
+import bc1.gream.domain.coupon.provider.CouponProvider;
 import bc1.gream.global.common.RestResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -19,14 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final CouponCommandService couponCommandService;
+    private final CouponProvider couponProvider;
 
     @PostMapping("/coupon")
     @Operation(summary = "쿠폰 생성요청 [어드민 ONLY]", description = "어드민 권한의 관리자의 쿠폰 생성 요청을 처리합니다.")
     public RestResponse<AdminCreateCouponResponseDto> createCoupon(
         @Valid @RequestBody AdminCreateCouponRequestDto adminCreateCouponRequestDto
     ) {
-        Coupon coupon = couponCommandService.createCoupon(adminCreateCouponRequestDto);
+        Coupon coupon = couponProvider.createCoupon(adminCreateCouponRequestDto);
         AdminCreateCouponResponseDto responseDto = CouponMapper.INSTANCE.toAdminCreateCouponResponseDto(coupon);
         return RestResponse.success(responseDto);
     }
