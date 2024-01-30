4274774dd7cd7a213018477e8a609551400aceb8
barami62 <wodbs7758@naver.com>
2024-01-30 12:12:29 +0900

    [#200] refactor : 환급 서비스의 추가로 컨트롤러의 의존 관계 변경

--- src/main/java/bc1/gream/domain/admin/controller/AdminController.java
+++ src/main/java/bc1/gream/domain/admin/controller/AdminController.java
@@ -3,8 +3,9 @@ package bc1.gream.domain.admin.controller;
 import bc1.gream.domain.admin.dto.request.AdminGetRefundRequestDto;
 import bc1.gream.domain.admin.dto.response.AdminGetRefundResponseDto;
 import bc1.gream.domain.admin.mapper.RefundMapper;
-import bc1.gream.domain.user.service.UserService;
+import bc1.gream.domain.user.service.RefundQueryService;
 import bc1.gream.global.common.RestResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final UserService userService;
+    private final RefundQueryService refundQueryService;
 
     @GetMapping("/refunds")
+    @Operation(summary = "신청된 환급 리스트 조회 요청", description = "사용자가 신청한 환급 요청 리스트를 반환합니다.")
     public RestResponse<List<AdminGetRefundResponseDto>> getRefunds(AdminGetRefundRequestDto requestDto) {
 
-        List<AdminGetRefundResponseDto> response = userService.getRefunds().stream()
+        List<AdminGetRefundResponseDto> response = refundQueryService.getRefunds()
+            .stream()
             .map(RefundMapper.INSTANCE::toAdminGetRefundResponseDto)
             .toList();
 
