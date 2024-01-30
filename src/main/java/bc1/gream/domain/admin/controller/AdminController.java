99746869c0f76701041b34cb1afcc0e9b9c14838
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-30 13:07:38 +0900

    chore : origin/main 에서의 AdminController 병합

--- src/main/java/bc1/gream/domain/admin/controller/AdminController.java
+++ src/main/java/bc1/gream/domain/admin/controller/AdminController.java
@@ -1,23 +1,23 @@
 package bc1.gream.domain.admin.controller;
 
 import bc1.gream.domain.admin.dto.request.AdminCreateCouponRequestDto;
-import bc1.gream.domain.admin.dto.response.AdminCreateCouponResponseDto;
 import bc1.gream.domain.admin.dto.request.AdminGetRefundRequestDto;
 import bc1.gream.domain.admin.dto.request.AdminProductRequestDto;
 import bc1.gream.domain.admin.dto.request.AdminRefundPassResponseDto;
+import bc1.gream.domain.admin.dto.response.AdminCreateCouponResponseDto;
 import bc1.gream.domain.admin.dto.response.AdminGetRefundResponseDto;
 import bc1.gream.domain.admin.dto.response.AdminProductResponseDto;
 import bc1.gream.domain.admin.mapper.RefundMapper;
-import bc1.gream.domain.product.service.query.ProductService;
-import bc1.gream.domain.user.service.command.RefundCommandService;
-import bc1.gream.domain.user.service.query.RefundQueryService;
 import bc1.gream.domain.coupon.entity.Coupon;
 import bc1.gream.domain.coupon.mapper.CouponMapper;
 import bc1.gream.domain.coupon.provider.CouponProvider;
+import bc1.gream.domain.product.service.query.ProductService;
+import bc1.gream.domain.user.service.command.RefundCommandService;
+import bc1.gream.domain.user.service.query.RefundQueryService;
 import bc1.gream.global.common.RestResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.List;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,6 +35,7 @@ public class AdminController {
     private final ProductService productService;
     private final RefundQueryService refundQueryService;
     private final RefundCommandService refundCommandService;
+    private final CouponProvider couponProvider;
 
     @GetMapping("/refunds")
     @Operation(summary = "신청된 환급 리스트 조회 요청", description = "사용자가 신청한 환급 요청 리스트를 반환합니다.")
@@ -57,7 +58,6 @@ public class AdminController {
         return RestResponse.success(new AdminProductResponseDto());
     }
 
-
     @DeleteMapping("/refund/{id}")
     @Operation(summary = "유저 환급 승인", description = "유저가 신청한 환급 요청을 승인해주는 기능입니다.")
     public RestResponse<AdminRefundPassResponseDto> approveRefund(
@@ -67,9 +67,6 @@ public class AdminController {
 
         return RestResponse.success(responseDto);
     }
-}
-
-    private final CouponProvider couponProvider;
 
     @PostMapping("/coupon")
     @Operation(summary = "쿠폰 생성요청 [어드민 ONLY]", description = "어드민 권한의 관리자의 쿠폰 생성 요청을 처리합니다.")
