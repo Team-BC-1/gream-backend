481409bcffea1d23a40669baa1772c79abc67954
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-30 12:01:42 +0900

    [#207]refactor: @Builder 추가

--- src/main/java/bc1/gream/domain/user/dto/request/UserPointRefundRequestDto.java
+++ src/main/java/bc1/gream/domain/user/dto/request/UserPointRefundRequestDto.java
@@ -1,5 +1,8 @@
 package bc1.gream.domain.user.dto.request;
 
+import lombok.Builder;
+
+@Builder    // 테스트용
 public record UserPointRefundRequestDto(
     Long point,
     String bank,
--- src/main/java/bc1/gream/domain/user/dto/response/UserPointRefundResponseDto.java
+++ src/main/java/bc1/gream/domain/user/dto/response/UserPointRefundResponseDto.java
@@ -1,5 +1,8 @@
 package bc1.gream.domain.user.dto.response;
 
+import lombok.Builder;
+
+@Builder
 public record UserPointRefundResponseDto(
     Long refundPoint,
     Long userPoint
