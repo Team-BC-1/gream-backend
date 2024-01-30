f93d6f6a3ce0668c7259da8f2b00342dfda80b05
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-01-30 10:59:16 +0900

    [#208]refactor: security filter 단에서 admin 권한을 확인하기 때문에 유저 어드민 유저 체크 기능 제거

--- src/main/java/bc1/gream/domain/admin/service/AdminService.java
+++ src/main/java/bc1/gream/domain/admin/service/AdminService.java
@@ -2,12 +2,8 @@ package bc1.gream.domain.admin.service;
 
 import bc1.gream.domain.admin.dto.request.AdminRefundPassResponseDto;
 import bc1.gream.domain.user.entity.Refund;
-import bc1.gream.domain.user.entity.User;
-import bc1.gream.domain.user.entity.UserRole;
 import bc1.gream.domain.user.repository.RefundRepository;
 import bc1.gream.domain.user.service.UserService;
-import bc1.gream.global.common.ResultCase;
-import bc1.gream.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,28 +14,12 @@ public class AdminService {
     private final UserService userService;
     private final RefundRepository refundRepository;
 
-    public AdminRefundPassResponseDto refundPass(User user, Long refundId) {
-
-        User AdminUser = checkUser(user.getLoginId());
+    public AdminRefundPassResponseDto refundPass(Long refundId) {
 
         Refund refund = userService.findRefund(refundId);
 
         refundRepository.delete(refund);
 
         return new AdminRefundPassResponseDto();
-    }
-
-    private User checkUser(String loginId) {
-        User user = userService.findUser(loginId);
-
-        if (isAdmin(user)) {
-            return user;
-        }
-
-        throw new GlobalException(ResultCase.NOT_AUTHORIZED);
-    }
-
-    private boolean isAdmin(User user) {
-        return user.getRole().equals(UserRole.ADMIN);
     }
 }
