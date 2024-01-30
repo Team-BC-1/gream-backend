e7bff8054922fe3af2d4b8b60750e89b3d1f8c8e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-30 19:50:49 +0900

    refactor : TransactionRoutingDataSource 주석처리

--- src/main/java/bc1/gream/global/config/datasource/TransactionRoutingDataSource.java
+++ src/main/java/bc1/gream/global/config/datasource/TransactionRoutingDataSource.java
@@ -1,19 +1,15 @@
 package bc1.gream.global.config.datasource;
 
 
-import jakarta.annotation.Nullable;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+public class TransactionRoutingDataSource {
+//    extends AbstractRoutingDataSource {
 
-public class TransactionRoutingDataSource
-    extends AbstractRoutingDataSource {
-
-    @Nullable
-    @Override
-    protected Object determineCurrentLookupKey() {
-        return TransactionSynchronizationManager
-            .isCurrentTransactionReadOnly() ?
-            DataSourceType.READ_ONLY :
-            DataSourceType.READ_WRITE;
-    }
+//    @Nullable
+//    @Override
+//    protected Object determineCurrentLookupKey() {
+//        return TransactionSynchronizationManager
+//            .isCurrentTransactionReadOnly() ?
+//            DataSourceType.READ_ONLY :
+//            DataSourceType.READ_WRITE;
+//    }
 }
