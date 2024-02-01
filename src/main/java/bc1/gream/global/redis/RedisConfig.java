2312cc744858065372d7604cd4125998ac04203d
barami62 <wodbs7758@naver.com>
2024-02-01 22:33:48 +0900

    [#224] feat : 복제본 환경 시 설정 추가

--- src/main/java/bc1/gream/global/redis/RedisConfig.java
+++ src/main/java/bc1/gream/global/redis/RedisConfig.java
@@ -1,26 +1,46 @@
 package bc1.gream.global.redis;
 
+import io.lettuce.core.ReadFrom;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+    @Value("${spring.data.redis.primary.host}")
+    private String primaryHost;
+
+    @Value("${spring.data.redis.replica.host}")
+    private String replicaHost;
 
     @Value("${spring.data.redis.port}")
     private Integer port;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        // 복제본이 없는 환경에서 실행 시
+        if (!StringUtils.hasText(replicaHost)) {
+            return new LettuceConnectionFactory(primaryHost, port);
+        }
+
+        // ElastiCache 이용하는 배포 환경에서 실행 시
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+            .readFrom(ReadFrom.REPLICA_PREFERRED) // Replica 우선 조회, 장애 시 Primary 조회
+            .build();
+
+        RedisStaticMasterReplicaConfiguration serverConfig = new RedisStaticMasterReplicaConfiguration(primaryHost, port);
+        serverConfig.addNode(replicaHost, port); // Primary 노드에 Replica 노드 추가
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
     @Bean
