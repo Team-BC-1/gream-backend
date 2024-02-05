d5aa796d8e4a38ea1f554337687c972adf63938b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-05 23:43:10 +0900

    feat : BuyRepository 단건조회 시, findById() -> findByIdAndDeadlineAtLessThan() 로 변경

--- src/main/java/bc1/gream/domain/buy/repository/BuyRepository.java
+++ src/main/java/bc1/gream/domain/buy/repository/BuyRepository.java
@@ -1,8 +1,14 @@
 package bc1.gream.domain.buy.repository;
 
 import bc1.gream.domain.buy.entity.Buy;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BuyRepository extends JpaRepository<Buy, Long>, BuyRepositoryCustom {
+
+    @Query("select b from Buy b where b.id = ?1 and b.deadlineAt <= ?2")
+    Optional<Buy> findByIdAndDeadlineAtLessThan(Long id, LocalDateTime deadlineAt);
 
 }
