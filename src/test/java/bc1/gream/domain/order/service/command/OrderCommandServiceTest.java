6390fb8f32dbe3c5de3b1ab2d8ffbb943f88b41a
Mayst <48579987+Mayst1232@users.noreply.github.com>
2024-02-06 19:00:24 +0900

    [#258]test: 구매로 인하여 새로운 주문이 생기는 서비스 기능 성공 테스트 추가

---
+++ src/test/java/bc1/gream/domain/order/service/command/OrderCommandServiceTest.java
@@ -0,0 +1,46 @@
+package bc1.gream.domain.order.service.command;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import bc1.gream.domain.order.entity.Order;
+import bc1.gream.domain.order.repository.OrderRepository;
+import bc1.gream.test.BuyTest;
+import bc1.gream.test.CouponTest;
+import bc1.gream.test.OrderTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class OrderCommandServiceTest implements OrderTest, BuyTest, CouponTest {
+
+    @InjectMocks
+    private OrderCommandService orderCommandService;
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Test
+    void 구매로_인하여_새로운_주문이_생기는_서비스_기능_성공_테스트() {
+        // given - when
+        orderCommandService.saveOrderOfBuy(TEST_BUY_COUPON, TEST_SELLER, TEST_COUPON_FIX);
+
+        // then
+        verify(orderRepository, times(1)).save(any(Order.class));
+    }
+
+    @Test
+    void saveOrderOfBuyNotCoupon() {
+    }
+
+    @Test
+    void saveOrderOfSell() {
+    }
+
+    @Test
+    void saveOrderOfSellNotCoupon() {
+    }
+}
