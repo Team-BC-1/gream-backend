75da8af486e09787cbd75154934084ec8d83f169
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-07 07:15:12 +0900

    feat : ProductCommandService 상품 저장 시, MultipartFile 이미지 저장

--- src/main/java/bc1/gream/domain/product/service/command/ProductCommandService.java
+++ src/main/java/bc1/gream/domain/product/service/command/ProductCommandService.java
@@ -5,6 +5,7 @@ import bc1.gream.domain.product.entity.Product;
 import bc1.gream.domain.product.repository.ProductRepository;
 import bc1.gream.global.common.ResultCase;
 import bc1.gream.global.exception.GlobalException;
+import bc1.gream.infra.s3.S3ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,24 +16,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductCommandService {
 
     private final ProductRepository productRepository;
+    private final S3ImageService s3ImageService;
 
     public void addProduct(AdminProductRequestDto adminProductRequestDto) {
+        verfiyExistenceByName(adminProductRequestDto);
+        Product product = buildProductFromRequest(adminProductRequestDto);
+        productRepository.save(product);
+    }
+
+    private void verfiyExistenceByName(AdminProductRequestDto adminProductRequestDto) {
         String productName = adminProductRequestDto.name();
 
         if (productRepository.existsByName(productName)) {
             throw new GlobalException(ResultCase.DUPLICATED_PRODUCT_NAME);
         }
-
-        Product product = buildProductFromRequest(adminProductRequestDto);
-        productRepository.save(product);
     }
 
     private Product buildProductFromRequest(AdminProductRequestDto adminProductRequestDto) {
+        String imageUrl = s3ImageService.getUrlAfterUpload(adminProductRequestDto.file());
         return Product.builder()
             .name(adminProductRequestDto.name())
             .brand(adminProductRequestDto.brand())
             .description(adminProductRequestDto.description())
-            .imageUrl(adminProductRequestDto.imageUrl())
+            .imageUrl(imageUrl)
             .price(adminProductRequestDto.price())
             .build();
     }
