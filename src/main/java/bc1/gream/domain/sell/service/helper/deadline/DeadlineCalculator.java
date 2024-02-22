aaab17bec1f8486a28684f79ef6dbb783b7562a3
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-22 19:36:09 +0900

    refactor : DeadlineCalculator 에 마감기한 구하기 메서드 추가

--- src/main/java/bc1/gream/domain/sell/service/helper/deadline/DeadlineCalculator.java
+++ src/main/java/bc1/gream/domain/sell/service/helper/deadline/DeadlineCalculator.java
@@ -1,10 +1,18 @@
 package bc1.gream.domain.sell.service.helper.deadline;
 
+import bc1.gream.domain.sell.dto.request.SellBidRequestDto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public final class DeadlineCalculator {
+
+    public static LocalDateTime getDeadlineOf(SellBidRequestDto requestDto) {
+        Integer period = Deadline.getPeriod(requestDto.period());
+        LocalDateTime deadlineAt = DeadlineCalculator.calculateDeadlineBy(LocalDate.now(), LocalTime.MAX,
+            period);
+        return deadlineAt;
+    }
 
     /**
      * 해당 날짜 date 의 시간대 time 로부터 마감일 period 후의 날짜 반환
