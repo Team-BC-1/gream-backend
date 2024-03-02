b7948629a0b02c1bdf4c860d63299dab972c612b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-03-02 23:43:43 +0900

    refactor : AsyncConfig 개선
    - 공식에 따른 corePoolSize 지정
    - saturation policy 지정
    - thread pool 에 대한 exception handler 지정

--- src/main/java/bc1/gream/global/config/async/AsyncConfig.java
+++ src/main/java/bc1/gream/global/config/async/AsyncConfig.java
@@ -1,6 +1,8 @@
-package bc1.gream.global.config;
+package bc1.gream.global.config.async;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
@@ -12,28 +14,31 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class AsyncConfig implements AsyncConfigurer {
+
+    private final GlobalAsyncExceptionHandler globalAsyncExceptionHandler;
 
     @Override
     @Bean(name = "tossPaymentsExecutor")
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1); // Set the core pool size. Default is 1.
-        executor.setMaxPoolSize(10); // Set the maximum pool size. Default is  Integer.MAX_VALUE.
-        executor.setQueueCapacity(10); // Set the capacity of the task queue. Default is Integer.MAX_VALUE.
+        int numOfCores = Runtime.getRuntime().availableProcessors();
+        float targetCpuUtilization = 0.3f;
+        float blockingCoefficient = 0.1f;
+        int corePoolSize = (int) (numOfCores * targetCpuUtilization * (1 + blockingCoefficient));
+        executor.setCorePoolSize(corePoolSize);
+        executor.setQueueCapacity(Integer.MAX_VALUE);
+        executor.setMaxPoolSize(Integer.MAX_VALUE);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setAllowCoreThreadTimeOut(true);
         executor.setThreadNamePrefix("custom-async-tosspay"); // Set the thread name prefix
-        executor.setRejectedExecutionHandler((r, executor1) ->
-            log.error("Task rejected : {} , thread pool is full and queue is also full : {}",
-                r.toString(),
-                executor1.getCorePoolSize()));
         executor.initialize();
         return executor;
     }
 
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return (ex, method, params) ->
-            log.error("Exception handler for async method : {} throw unexpected exception {}",
-                method.toGenericString(), ex);
+        return globalAsyncExceptionHandler;
     }
 }
